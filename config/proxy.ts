95d1e1da2cad9d7336427d4bee955766fc3e2e62
Kevin <wcdma020@hotmail.com>
2022-04-25 01:03:25 +0800

    设置代理地址

--- config/proxy.ts
+++ config/proxy.ts
@@ -8,27 +8,24 @@
  */
 export default {
   dev: {
-    // localhost:8000/api/** -> https://preview.pro.ant.design/api/**
     '/api/': {
-      // 要代理的地址
-      target: 'https://preview.pro.ant.design',
-      // 配置了这个可以从 http 代理到 https
-      // 依赖 origin 的功能可能需要这个，比如 cookie
+      target: 'https://demo.brandsz.cn/console',
       changeOrigin: true,
-    },
-  },
-  test: {
-    '/api/': {
-      target: 'https://proapi.azurewebsites.net',
-      changeOrigin: true,
-      pathRewrite: { '^': '' },
+      pathRewrite: { '/api': '' },
     },
   },
   pre: {
     '/api/': {
-      target: 'your pre url',
+      target: 'https://www.brandsz.cn/console',
       changeOrigin: true,
-      pathRewrite: { '^': '' },
+      pathRewrite: { '/api': '' },
     },
   },
-};
+  test: {
+    '/api/': {
+      target: 'https://test.brandsz.cn/console',
+      changeOrigin: true,
+      pathRewrite: { '/api': '' },
+    },
+  }
+}
