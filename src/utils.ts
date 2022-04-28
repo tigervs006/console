fc0e6ec047dfa12d85de1f7b0dcf1ffe4ec312c5
Kevin <wcdma020@hotmail.com>
2022-04-28 11:37:54 +0800

    新增utils工具类

---
+++ src/utils.ts
@@ -0,0 +1,11 @@
+/**
+ * 延时返回结果
+ * @param time 毫秒
+ */
+export const waitTime = (time: number = 100) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(true)
+    }, time)
+  })
+};
