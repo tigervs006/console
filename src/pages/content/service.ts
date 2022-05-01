95e623110e90f2cddb8434896c4910d4ab903bc4
Kevin <wcdma020@hotmail.com>
2022-05-01 17:08:11 +0800

    新增删除接口

--- src/pages/content/service.ts
+++ src/pages/content/service.ts
@@ -1,14 +1,18 @@
 import type { status } from './data';
-import { list, update } from "@/services/ant-design-pro/api";
+import { getData, postData } from "@/services/ant-design-pro/api";
 
 export async function getAuthor() {
-  return list('/article/author')
+  return getData('/article/author')
+}
+
+export async function remove(data: Record<string, any>) {
+  return postData('/article/del', { ...data })
 }
 
 export async function fetchData(params: API.PageParams) {
-  return list('/article/list', { ...params })
+  return getData('/article/list', { ...params })
 }
 
 export async function setStatus(data: status) {
-  return update('/article/status', { ...data })
+  return postData('/article/status', { ...data })
 }
