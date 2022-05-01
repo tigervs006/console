677ff91e587095d81f6a19226202e41496641829
Kevin <wcdma020@hotmail.com>
2022-05-01 23:03:46 +0800

    remove方法类型调整

--- src/pages/content/service.ts
+++ src/pages/content/service.ts
@@ -1,18 +1,18 @@
-import type { status } from './data';
+import type { statusData } from './data';
 import { getData, postData } from "@/services/ant-design-pro/api";
 
 export async function getAuthor() {
   return getData('/article/author')
 }
 
-export async function remove(data: Record<string, any>) {
-  return postData('/article/del', { ...data })
-}
-
 export async function fetchData(params: API.PageParams) {
   return getData('/article/list', { ...params })
 }
 
-export async function setStatus(data: status) {
+export async function setStatus(data: statusData) {
   return postData('/article/status', { ...data })
+}
+
+export async function remove(data: { id: (string | number)[] }) {
+  return postData('/article/del', { ...data })
 }
