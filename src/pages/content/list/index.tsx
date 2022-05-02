ea45bfd8349013fe2535a42df1092f91386aaf8e
Kevin <wcdma020@hotmail.com>
2022-05-02 10:39:14 +0800

    完成删除的基础功能

--- src/pages/content/list/index.tsx
+++ src/pages/content/list/index.tsx
@@ -1,59 +1,39 @@
 import '../index.less';
-import { useState } from 'react';
+import {useRequest} from 'umi';
+import {useRef, useState} from 'react';
+import type {ProColumns} from '@ant-design/pro-table';
 import ProTable from '@ant-design/pro-table';
-import { Switch, Button, message } from 'antd';
 import {PageContainer} from '@ant-design/pro-layout';
-import type { ProColumns } from '@ant-design/pro-table';
-import { list, update } from '@/services/ant-design-pro/api';
-import { SearchOutlined, EditOutlined, DeleteOutlined } from '@ant-design/icons';
+import type {DataItem, valueEnumData} from "../data";
+import {Button, message, Switch, Space, Table} from 'antd';
+import {fetchData, getAuthor, remove, setStatus} from "../service";
+import {DeleteOutlined, EditOutlined, SearchOutlined} from '@ant-design/icons';
 
-type valueEnum = {
-  [key: string]: {
-    text: string
-    status: string
-  }
+interface ActionType {
+  reload: (resetPageIndex?: boolean) => void;
+  reloadAndRest: () => void;
+  reset: () => void;
+  clearSelected?: () => void;
+  startEditable: (rowKey: number) => boolean;
+  cancelEditable: (rowKey: number) => boolean;
 }
 
-type TableListItem = {
-  id: number
-  cid: number
-  click: number
-  title: number
-  author: string
-  status: number
-  channel: string
-  is_head: number
-  is_recom: number
-  is_collect: number
-  create_time: string
-  update_time: string
-};
-
 export default () => {
+  // 重载表格
+  const ref: any = useRef<ActionType>();
 
-  const valueEnums = {
-    kevin: {
-      text: '甘文锋',
-      status: 'kevin'
-    },
-    tyler: {
-      text: '曹攀峰',
-      status: 'tyler'
-    },
-    anonymous: {
-      text: '匿名者',
-      status: 'anonymous'
-    }
-  };
+  // 文章作者
+  const [valueEnum , setValueEnum] = useState<valueEnumData>()
+  useRequest(getAuthor, {onSuccess: (res: { list: Record<string, any> }) => {
+    const newObj: Record<string, any> = {}
+    res.list.map((item: Record<string, any>) => {
+      newObj.anonymous = {text: '佚名', status: 'anonymous'}
+      newObj[item.name] = {text: item.cname, status: item.name}
+    })
+    setValueEnum(newObj)
+  }})
 
-  /**
-   * switch loading...
-   */
-  const [switchLoading, setSwitchLoading] = useState<boolean>(false)
-
-  const [valueEnum, setValueEnum] = useState<valueEnum>(valueEnums)
-
-  const columns: ProColumns<TableListItem>[] = [
+  const columns: ProColumns[] = [
     {
       title: 'ID',
       dataIndex: 'id'
@@ -63,6 +43,7 @@ export default () => {
       search: false,
       filters: true,
       onFilter: true,
+      filterMode: 'tree',
       dataIndex: 'author',
       valueType: 'select',
       valueEnum: {...valueEnum}
@@ -98,10 +79,10 @@ export default () => {
       search: false,
       dataIndex: 'status',
       render: (text, record) => [
-        // @ts-ignore
+        // FIXME: 不能单独控制每个switch的loading状态
         // eslint-disable-next-line @typescript-eslint/no-use-before-define
-        <Switch key="id" loading={switchLoading} checkedChildren="显示" unCheckedChildren="隐藏" defaultChecked={!!record.status} onChange={(checked) => handleChange(checked, record.id)} />
-      ],
+        <Switch key="id" loading={record.loading} checkedChildren="显示" unCheckedChildren="隐藏" defaultChecked={!!record.status} onChange={(checked) => handleChange(checked, record)} />
+      ]
     },
     {
       title: '操作',
@@ -122,22 +103,24 @@ export default () => {
 
   /**
    * 删除文章
-   * @param id 文章id
+   * @param id id
    */
-  const handleDelete = (id: number) => {
-    console.log(`当前要删除的文章ID为${id}`)
+  const handleDelete = async (id: (string | number)[]) => {
+    const res = await remove({ id: id })
+    if (res.success) {
+      ref.current.reload()
+    }
+    message.success(res.msg)
   }
 
   /**
    * 设置文章状态
-   * @param id 文章id
    * @param checked 状态
+   * @param record 单条数据
    */
-  const handleChange = async (checked: boolean, id: number) => {
-    setSwitchLoading(true)
-    const res = await update('/article/status', { id: id, status: checked ? 1 : 0 })
-    setSwitchLoading(false)
-    if (res !== undefined || true) message.success(res.msg)
+  const handleChange = async (checked: boolean, record: Record<string, any>) => {
+    const res = await setStatus({ id: record.id, status: checked ? 1 : 0 })
+    message.success(res.msg)
   }
 
   /**
@@ -145,7 +128,7 @@ export default () => {
    * @param id 文章id
    */
   const handleEdit = (id: number) => {
-    console.log(`当前要编辑的文章ID为：${id}`)
+    console.log(`当前要编辑的文章ID为：`, id)
   }
 
   /**
@@ -159,29 +142,60 @@ export default () => {
   /**
    * 获取文章列表
    * @param params 参数
-   * @param sort 排序
+   * @param sort   排序
    * @param filter 筛选
    */
-  const fetchData = async (params: API.PageParams, sort: any, filter: any) => {
+  const tableData = async (params: API.PageParams, sort: any, filter: any) => {
     const paramData = Object.assign({ ...params }, sort, filter)
     // 过滤空值对象，比后台做判断更方便
     for (const idx in paramData) {
-      if (paramData[idx] === '' ||　paramData[idx] === null || paramData[idx] === undefined) delete paramData[idx]
+      if ('' === paramData[idx] ||　null === paramData[idx] || undefined === paramData[idx]) delete paramData[idx]
     }
-    const res = await list('/article/list', paramData);
+    const res = await fetchData({ ...paramData })
     return {
+      data: res.data.list,
       success: res.success,
-      data: res.result.data,
-      total: res.result.total
+      total: res.data.total
     }
+  }
+
+  /**
+   * 处理request
+   * @param data data
+   */
+  const postDatas = (data: Record<string, any>) => {
+    // 添加loading元素到每条数据
+    // 以单独控制每个switch按钮的loading状态
+    return data.map((item: DataItem[]) => ({
+      ...item,
+      'loading': false
+    }))
   }
 
   return (
     <PageContainer>
-      <ProTable<TableListItem>
+      <ProTable
         rowKey="id"
+        actionRef={ref}
         columns={columns}
-        request={fetchData}
+        request={tableData}
+        postData={postDatas}
+        rowSelection={{
+          selections: [Table.SELECTION_ALL, Table.SELECTION_INVERT]
+        }}
+        tableAlertRender={({ selectedRowKeys, selectedRows, onCleanSelected }) => (
+          <Space size={24}>
+          <span>已选 {selectedRowKeys.length} 项<a style={{ marginLeft: 8 }} onClick={onCleanSelected}>取消选择</a></span>
+          <span>浏览量 {selectedRows && selectedRows.reduce((pre, item) => pre + item.click, 0)} 次</span>
+          </Space>
+        )}
+        tableAlertOptionRender={({ selectedRowKeys }) => {
+          return (
+            <Space size={16}>
+              <a onClick={() => handleDelete(selectedRowKeys)}>批量删除</a>
+            </Space>
+          )
+        }}
       />
     </PageContainer>
   )
