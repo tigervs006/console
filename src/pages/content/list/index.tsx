d4049761c4f4b2f5329f46f6ad622c02a07fb177
Kevin <wcdma020@hotmail.com>
2022-05-05 01:24:12 +0800

    fix(compiler): 删除create_at多余变量

--- src/pages/content/list/index.tsx
+++ src/pages/content/list/index.tsx
@@ -358,7 +358,6 @@ export default () => {
           syncToUrl: (values) => {
             return {
               ...values,
-              created_at: [values.startTime, values.endTime],
             };
           },
         }}
