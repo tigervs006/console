0d22d8956976c705f5785f5b79d46f1235d7254d
Kevin <wcdma020@hotmail.com>
2022-05-06 22:34:50 +0800

    chore(compiler): 调整文件名及移动位置到utils文件夹

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -5,7 +5,7 @@
 export const waitTime = (time: number = 100) => {
   return new Promise((resolve) => {
     setTimeout(() => {
-      resolve(true)
-    }, time)
-  })
+      resolve(true);
+    }, time);
+  });
 };
