e7f5cf2ad722c043eb1d63ec350093707a54fc71
Kevin <wcdma020@hotmail.com>
2022-05-07 14:27:31 +0800

    chore(Kevin): 新增若干参数类型

--- src/pages/components/Ckeditor/data.d.ts
+++ src/pages/components/Ckeditor/data.d.ts
@@ -1,5 +1,8 @@
 export interface stateData {
+  saving: boolean;
   content: string;
+  defaultType: string;
+  defaultStatus: string;
 }
 export interface parentProps {
   dataContent: (event: any, contents: string) => void;
