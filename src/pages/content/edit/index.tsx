ac51414032e5981c33bc951a080662a360057391
Kevin <wcdma020@hotmail.com>
2022-05-09 12:13:41 +0800

    feat(Kevin): 完成表单的基本功能及布局

--- src/pages/content/edit/index.tsx
+++ src/pages/content/edit/index.tsx
@@ -1,35 +1,217 @@
-import { useRequest } from 'umi';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import { waitTime } from '@/utils/tools';
 import Ckeditor from '@/pages/components/Ckeditor';
 import { getChannel } from '@/pages/content/service';
 import { PageContainer } from '@ant-design/pro-layout';
-import type { channelDataItem, channelOptions } from '../data';
+import type { articleData, channelDataItem } from '../data';
+import type { ProFormInstance } from '@ant-design/pro-form';
+import { notification, message, Space, Row, Col } from 'antd';
+import { UndoOutlined, FormOutlined } from '@ant-design/icons';
+import ProForm, {
+  ProFormText,
+  ProFormSelect,
+  ProFormTextArea,
+  ProFormCheckbox,
+  ProFormUploadButton,
+} from '@ant-design/pro-form';
 
 export default () => {
+  const formRef = useRef<ProFormInstance>();
   // 文章内容
+  // @ts-ignore
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [content, setContent] = useState<string>('');
   // 新闻栏目
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [channelOptions, setChannelOptions] = useState<channelOptions[]>();
-  // 获取新闻栏目
-  useRequest(getChannel, {
-    onSuccess: (res: { list: channelDataItem[] }) => {
-      const channel = res?.list.map((item: channelDataItem) => ({
-        label: item.cname,
-        value: item.id,
-      }));
-      setChannelOptions(channel);
-    },
-  });
-
-  const getContent = (e: any, contents: string) => {
-    setContent(contents);
-    console.log('parentContent:', e.name, content);
+  const channel = async () => {
+    const res = await getChannel();
+    return res?.data?.list.map(
+      (item: channelDataItem) =>
+        ({
+          value: item.id,
+          label: item.cname,
+        } ?? {}),
+    );
   };
-
+  // 获得编辑器内容
+  const getContent = (e: any, contents: string) => setContent(contents);
   return (
     <PageContainer>
-      <Ckeditor setContent={getContent} />
+      <ProForm<articleData>
+        formRef={formRef}
+        submitter={{
+          render: (_, doms) => (
+            <Row>
+              <Col offset={7}>
+                <Space size="middle">{doms}</Space>
+              </Col>
+            </Row>
+          ),
+          resetButtonProps: { shape: 'round', icon: <UndoOutlined /> },
+          submitButtonProps: { type: 'primary', shape: 'round', icon: <FormOutlined /> },
+        }}
+        onFinish={async (values) => {
+          await waitTime(2000);
+          console.log(values);
+          message.success('提交成功');
+        }}
+        // request参数
+        params={{ id: 125 }}
+        // 编辑文章时用
+        request={async (params) => {
+          console.log('params', params);
+          return new Promise((resolve) => {
+            setTimeout(() => {
+              resolve({
+                cid: 4,
+                title: 'I am title',
+                attribute: ['is_recom'],
+                keywords: 'I am keywords',
+                description: 'I am description',
+                litpic: ['https://cdn.tigervs.com/images/2020-12-03/5fc863031e5c9.png'],
+              });
+            }, 1000);
+          });
+        }}
+        // 表单默认值
+        initialValues={{
+          cid: 4,
+          title: '我是标题',
+          keywords: '我是关键词',
+          attribute: ['is_recom'],
+          description: '我是文章简述',
+          litpic: ['https://cdn.tigervs.com/images/2020-12-03/5fc863031e5c9.png'],
+        }}
+        // 监听输入变化
+        // onValuesChange={(changeValues) => console.log(changeValues)}
+      >
+        <ProFormSelect
+          width="sm"
+          name="cid"
+          label="栏目"
+          tooltip="文章发布的栏目"
+          request={channel}
+          rules={[{ required: true, message: '选择文章发布的栏目' }]}
+        />
+        <ProFormText
+          width="xl"
+          name="title"
+          label="标题"
+          tooltip="限制32个字符"
+          placeholder="请输入文章标题"
+          fieldProps={{ showCount: true, maxLength: 32 }}
+          rules={[
+            { required: true, message: '请输入文章标题' },
+            { min: 15, message: '文章标题不宜太短' },
+          ]}
+        />
+        <ProFormText
+          width="xl"
+          name="keywords"
+          label="关键词"
+          tooltip="请用空格分隔"
+          placeholder="请输入关键词"
+          fieldProps={{ showCount: true, maxLength: 64 }}
+          rules={[
+            { required: true, message: '请输入关键词' },
+            { min: 10, message: '再多来两个关键词' },
+          ]}
+        />
+        <ProFormTextArea
+          width="xl"
+          name="description"
+          label="文章简述"
+          tooltip="SEO优化很重要"
+          placeholder="请输入名称文章简述"
+          fieldProps={{
+            allowClear: true,
+            showCount: true,
+            maxLength: 256,
+            autoSize: { minRows: 5, maxRows: 8 },
+          }}
+          rules={[
+            { required: true, message: '请输入文章文章简述' },
+            { min: 50, message: '再多几句文章简述', type: 'string' },
+          ]}
+        />
+        <ProFormUploadButton
+          max={1}
+          width="xl"
+          label="文档封面"
+          name="litpic"
+          title="Upload"
+          action="upload.do"
+          tooltip="仅支持png、jpg、jpeg"
+          rules={[
+            { required: true, message: '文档封面必须得有', type: 'array' },
+            { max: 1, message: '文档封面只要一张就行了', type: 'array' },
+          ]}
+          fieldProps={{
+            beforeUpload: (file) => {
+              const MAX_FILE_SIZE = 2;
+              const UNIT = 1024 * 1024;
+              const curType = file.type;
+              const fileType = ['image/png', 'image/jpeg', 'image/pjpeg'];
+              return new Promise((resolve, reject) => {
+                const reader = new FileReader();
+                reader.readAsDataURL(file as Blob);
+                reader.onload = function (e) {
+                  const base64: string | ArrayBuffer | null | undefined = e.target?.result;
+                  const image = document.createElement('img');
+                  // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+                  typeof base64 === 'string' ? (image.src = base64) : undefined;
+                  image.onload = function () {
+                    if (!fileType.includes(curType)) {
+                      notification.error({
+                        message: '上传的文件类型错误',
+                        description: `请上传格式为${fileType}的图片`,
+                      });
+                      reject();
+                    } else if (file.size > MAX_FILE_SIZE * UNIT) {
+                      notification.error({
+                        message: '图像大小不符合要求',
+                        description: `单张图像大小不得超过${MAX_FILE_SIZE}M`,
+                      });
+                      reject();
+                    } else if (750 > image.width && 450 > image.height) {
+                      notification.error({
+                        message: '图像尺寸不符合要求',
+                        description: `当前图像尺寸为：${image.width}X${image.height}，建议尺寸应为：≥750X450`,
+                      });
+                      reject();
+                    } else {
+                      resolve();
+                    }
+                  };
+                };
+              });
+            },
+            accept: '.png, .jpg, .jpeg, .gif',
+            listType: 'picture-card',
+            headers: { Authorization: localStorage.getItem('ACCESS_TOKEN') || '' },
+          }}
+        />
+        <ProFormCheckbox.Group
+          width="xl"
+          name="attribute"
+          label="文档属性"
+          tooltip="选择文档属性"
+          options={[
+            { label: '头条', value: 'is_head' },
+            { label: '推荐', value: 'is_recom' },
+            { label: '图文', value: 'is_litpic' },
+          ]}
+          rules={[
+            { required: true, message: '请您你设置文档属性', type: 'array' },
+            { min: 1, message: '请至少选择一个文档属性', type: 'array' },
+          ]}
+        />
+        {/* ckeditor5编辑器 */}
+        <Row>
+          <Col sm={24} md={12} lg={16}>
+            <Ckeditor setContent={getContent} />
+          </Col>
+        </Row>
+      </ProForm>
     </PageContainer>
   );
 };
