9fa13618975d94fa6faf34746eeda6b9daa9d7b6
Kevin <wcdma020@hotmail.com>
2022-05-13 02:42:13 +0800

    feat(Kevin): 新增提取正文第一张图片方法

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -1,5 +1,6 @@
 /**
  * 延时返回结果
+ * @return Promise
  * @param time 毫秒
  */
 export const waitTime = (time: number = 100) => {
@@ -8,4 +9,20 @@ export const waitTime = (time: number = 100) => {
       resolve(true);
     }, time);
   });
+};
+
+/**
+ * 提取文章图片
+ * @return array
+ * @param content
+ */
+export const extractImg = (content: string) => {
+  const images = [];
+  const srcReg = /src=[\'\"]?([^\'\"]*)[\'\"]?/i;
+  const imgArr = content.match(/<img.*?(?:>|\/>)/gi);
+  for (const idx in imgArr) {
+    const src = imgArr[idx].match(srcReg);
+    images.push(src?.[1] ?? undefined);
+  }
+  return images;
 };
