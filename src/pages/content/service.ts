ab2f288007c97583e214bb33e1bfcfed068c53be
Kevin <wcdma020@hotmail.com>
2022-05-15 23:04:02 +0800

    chore(Kevin): 新增getContent（获取文档内容）、saveContent（新增/编辑文档内容）接口

--- src/pages/content/service.ts
+++ src/pages/content/service.ts
@@ -1,22 +1,37 @@
-import type { statusData } from './data';
-import { getData, postData } from "@/services/ant-design-pro/api";
+import type { articleData, statusData } from './data';
+import { getData, postData } from '@/services/ant-design-pro/api';
 
+// 获取文档作者
 export async function getAuthor() {
-  return getData('/article/author')
+  return getData('/article/author');
 }
 
+// 获取新闻栏目
 export async function getChannel() {
-  return getData('/article/channel')
+  return getData('/article/channel');
 }
 
+// 新增/编辑内容
+export async function saveContent(data: articleData) {
+  return postData('/article/save', { ...data });
+}
+
+// 获取文档列表
 export async function fetchData(params: API.PageParams) {
-  return getData('/article/list', { ...params })
+  return getData('/article/list', { ...params });
 }
 
+// 修改文档状态
 export async function setStatus(data: statusData) {
-  return postData('/article/status', { ...data })
+  return postData('/article/status', { ...data });
 }
 
+// 获取文章内容
+export async function getContent(params: Record<string, any>) {
+  return getData('/article', { ...params });
+}
+
+// 单个/批量删除
 export async function remove(data: { id: (string | number)[] }) {
-  return postData('/article/del', { ...data })
+  return postData('/article/del', { ...data });
 }
