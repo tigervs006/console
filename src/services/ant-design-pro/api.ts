7c50ccfb45f79e9db48044803c89adc24d5d13e8
Kevin <wcdma020@hotmail.com>
2022-05-27 20:31:42 +0800

    feat(Kevin): 调整登录和登出、当前用户接口地址

--- src/services/ant-design-pro/api.ts
+++ src/services/ant-design-pro/api.ts
@@ -1,27 +1,29 @@
 import { request } from 'umi';
 import { stringify } from 'qs';
 
-/** 获取当前的用户 GET /api/currentUser */
-export async function currentUser(options?: Record<string, any>) {
+/** 获取当前的用户 GET /user */
+export async function currentUser(params: { id: string }, options?: Record<string, any>) {
   return request<{
     data: API.CurrentUser;
-  }>('/api/currentUser', {
+  }>('/user', {
     method: 'GET',
+    params: { ...params },
     ...(options || {}),
   });
 }
 
-/** 退出登录接口 POST /api/login/outLogin */
-export async function outLogin(options?: Record<string, any>) {
-  return request<Record<string, any>>('/api/login/outLogin', {
+/** 退出登录接口 POST /public/logout */
+export async function outLogin(data: { name: string }, options?: Record<string, any>) {
+  return request<Record<string, any>>('/public/logout', {
     method: 'POST',
+    data: { ...data },
     ...(options || {}),
   });
 }
 
-/** 登录接口 POST /api/login/account */
+/** 登录接口 POST /public/login */
 export async function login(body: API.LoginParams, options?: Record<string, any>) {
-  return request<API.LoginResult>('/api/login/account', {
+  return request<API.LoginResult>('/public/login', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -40,31 +42,38 @@ export async function getNotices(options?: Record<string, any>) {
 }
 
 /**
- * POST方法提交/获取数据
- * @param path 路径
- * @param data 数据
- * @param options options
- */
-export async function postData(path: string, data: Record<string, any>, options?: Record<string, any>) {
-  return request<Record<string, any>>(path, {
-    method: 'POST',
-    data: { ...data },
-    ...(options || {})
-  })
-}
-
-/**
  * GET 方法提交/获取数据
  * @param path 路径
  * @param params 参数
  * @param options options
  */
-export async function getData(path: string, params?: Record<string, any>, options?: Record<string, any>) {
+export async function getData(
+  path: string,
+  params?: Record<string, any>,
+  options?: Record<string, any>,
+) {
   return request<Record<string, any>>(path, {
     method: 'GET',
     params: { ...params },
     ...(options || {}),
-    // eslint-disable-next-line @typescript-eslint/no-shadow
-    paramsSerializer: (params) => stringify(params, { arrayFormat: 'indices' })
-  })
+    paramsSerializer: () => stringify(params, { arrayFormat: 'indices' }),
+  });
+}
+
+/**
+ * POST方法提交/获取数据
+ * @param path 路径
+ * @param data 数据
+ * @param options options
+ */
+export async function postData(
+  path: string,
+  data: Record<string, any>,
+  options?: Record<string, any>,
+) {
+  return request<Record<string, any>>(path, {
+    method: 'POST',
+    data: { ...data },
+    ...(options || {}),
+  });
 }
