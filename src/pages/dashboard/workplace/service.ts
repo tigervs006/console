c37c88cbeccd8ec125ca41c27d3620223457e68d
Kevin <wcdma020@hotmail.com>
2022-05-28 14:02:59 +0800

    feat(Kevin): service访问地址调整

--- src/pages/dashboard/workplace/service.ts
+++ src/pages/dashboard/workplace/service.ts
@@ -2,13 +2,13 @@ import { request } from 'umi';
 import type { NoticeType, ActivitiesType, AnalysisData } from './data';
 
 export async function queryProjectNotice(): Promise<{ data: NoticeType[] }> {
-  return request('/api/project/notice');
+  return request('/dashboard/notice');
 }
 
 export async function queryActivities(): Promise<{ data: ActivitiesType[] }> {
-  return request('/api/activities');
+  return request('/dashboard/activities');
 }
 
 export async function fakeChartData(): Promise<{ data: AnalysisData }> {
-  return request('/api/fake_workplace_chart_data');
+  return request('/dashboard/workplace');
 }
