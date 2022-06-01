f1840fb18d055b9b88e225ffc033aa95be0d0474
Kevin <wcdma020@hotmail.com>
2022-06-02 00:10:32 +0800

    chore(Kevin): 设置所有参数类型为可选

--- src/pages/user/data.d.ts
+++ src/pages/user/data.d.ts
@@ -1,12 +1,12 @@
 export type tableDataItem = {
-  id: number;
-  gid: number;
-  name: string;
-  cname: string;
-  status: number;
-  email: string;
-  avatar: string;
-  ipaddress: number;
-  last_login: string;
-  create_time: string;
+  id?: number;
+  gid?: number;
+  name?: string;
+  cname?: string;
+  status?: number;
+  email?: string;
+  avatar?: string;
+  ipaddress?: number;
+  last_login?: string;
+  create_time?: string;
 };
