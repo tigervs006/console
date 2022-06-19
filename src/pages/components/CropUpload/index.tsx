3b8a0b44d681360da7cd541caf7623528f9ec1ff
Kevin <wcdma020@hotmail.com>
2022-06-19 14:50:10 +0800

    chore(Kevin): 使用ImagePreview组件进行图片预览

--- src/pages/components/CropUpload/index.tsx
+++ src/pages/components/CropUpload/index.tsx
@@ -1,214 +1,201 @@
-import React from 'react';
-import 'antd/es/slider/style';
-import { useModel } from 'umi';
-import ImgCrop from 'antd-img-crop';
-import ProForm from '@ant-design/pro-form';
+/** @format */
+
+import { ImagePreview } from '@/pages/components/ImagePreview';
 import { removeFile } from '@/services/ant-design-pro/api';
-import { Button, message, Modal, notification, Upload } from 'antd';
-import type { UploadFile, UploadListType } from 'antd/es/upload/interface';
-import type { RcFile, UploadChangeParam, UploadProps } from 'antd/es/upload';
 import { CloudUploadOutlined, QuestionCircleOutlined } from '@ant-design/icons';
+import ProForm from '@ant-design/pro-form';
+import { Button, message, Modal, notification, Upload } from 'antd';
+import ImgCrop from 'antd-img-crop';
+import 'antd/es/slider/style';
+import type { RcFile, UploadChangeParam, UploadProps } from 'antd/es/upload';
+import type { UploadFile, UploadListType } from 'antd/es/upload/interface';
+import React, { useRef } from 'react';
+import { useModel } from 'umi';
 
 export const CropUpload: React.FC<
-  API.uploadComponents & {
-    cropAspect?: number;
-    setFieldsValue: (fileList: UploadFile[]) => void;
-  }
-> = (props) => {
-  const { confirm } = Modal;
-  // 大小限制
-  const fileSize: number = props?.fileSize ?? 2;
-  // 数量限制
-  const maxUpload: number = props?.maxUpload ?? 1;
-  // 裁剪比例
-  const aspect: number = props?.cropAspect ?? 1 / 1;
-  // 图片宽度
-  const imageWidth: number = props?.imageWidth ?? 750;
-  // 图片高度
-  const imageHeight: number = props?.imageHeight ?? 422;
-  // 按钮文字
-  const uploadText: string = props?.formTitle ?? '上传图像';
-  // 展示方式
-  const listType: UploadListType = props?.listType ?? 'picture-card';
-  // 文件后缀
-  const acceptFile: string = props?.acceptFile ?? '.png, .jpg, .jpeg, .gif';
-  // 文件列表
-  const { fileLists, setFileList } = useModel('file', (ret) => ({
-    fileLists: ret.fileList,
-    setFileList: ret.setFileList,
-  }));
-  // 文件格式
-  const fileType: string[] = props?.fileType ?? ['image/png', 'image/jpeg', 'image/gif'];
-
-  // 处理文件预览
-  const handlePreview = async (file: UploadFile) => {
-    let src = file.url as string;
-    if (!src) {
-      src = await new Promise((resolve) => {
-        const reader = new FileReader();
-        reader.readAsDataURL(file.originFileObj as RcFile);
-        reader.onload = () => resolve(reader.result as string);
-      });
+    API.uploadComponents & {
+        cropAspect?: number;
+        setFieldsValue: (fileList: UploadFile[]) => void;
     }
-    const image = new Image();
-    image.src = src;
-    const imgWindow = window.open(src);
-    imgWindow?.document.write(image.outerHTML);
-  };
+> = props => {
+    const { confirm } = Modal;
+    // 大小限制
+    const fileSize: number = props?.fileSize ?? 2;
+    // 数量限制
+    const maxUpload: number = props?.maxUpload ?? 1;
+    // 裁剪比例
+    const aspect: number = props?.cropAspect ?? 1 / 1;
+    // 图片宽度
+    const imageWidth: number = props?.imageWidth ?? 750;
+    // 图片预览
+    const previewRef: React.ForwardedRef<any> = useRef();
+    // 图片高度
+    const imageHeight: number = props?.imageHeight ?? 422;
+    // 按钮文字
+    const uploadText: string = props?.formTitle ?? '上传图像';
+    // 展示方式
+    const listType: UploadListType = props?.listType ?? 'picture-card';
+    // 文件后缀
+    const acceptFile: string = props?.acceptFile ?? '.png, .jpg, .jpeg, .gif';
+    // 文件列表
+    const { fileLists, setFileList } = useModel('file', ret => ({
+        fileLists: ret.fileList,
+        setFileList: ret.setFileList,
+    }));
+    // 文件格式
+    const fileType: string[] = props?.fileType ?? ['image/png', 'image/jpeg', 'image/gif'];
 
-  // 处理文件删除状态
-  const handleRemove: UploadProps['onRemove'] = (file: UploadFile) => {
-    return new Promise<boolean>((resolve, reject) => {
-      const url = file?.url ?? '';
-      // 从网址中截取文件的相对路径
-      const idx = url.lastIndexOf('.cn/');
-      const filePath = url.substring(idx + 4, url.length);
-      confirm({
-        centered: true,
-        cancelText: '算了',
-        title: '当真要删除?',
-        icon: <QuestionCircleOutlined />,
-        cancelButtonProps: { shape: 'round' },
-        okButtonProps: { danger: true, shape: 'round' },
-        content: url.match(/\/(\w+\.(?:png|jpg|gif|bmp))$/i)?.[1],
-        async onOk() {
-          const res = await removeFile({ filePath: filePath });
-          if (res.success) {
-            resolve(true);
-          } else {
-            reject('Failed');
-          }
-        },
-        onCancel() {
-          reject('onCancel');
-        },
-      });
-    });
-  };
-
-  // 处理上传事件
-  const handleChange: UploadProps['onChange'] = (info: UploadChangeParam) => {
-    const { fileList } = info;
-    setFileList(fileList.slice());
-    const status = info.file.status;
-    switch (status) {
-      case 'uploading':
-        message.info!('File is uploading...');
-        break;
-      case 'done':
-        setFileList([
-          Object.assign(
-            { ...info.file.response.data },
-            { status: info.file.response.success ? 'done' : 'error' },
-          ),
-        ]);
-        props.setFieldsValue(fileList);
-        message.success!(info.file.response.msg);
-        break;
-      case 'success':
-        message.success!('File uploaded successfully');
-        break;
-      case 'removed':
-        message.success!('File removed successfully');
-        break;
-      case 'error':
-        notification.error({
-          message: 'Error',
-          description: info.file?.response?.msg ?? 'File upload failed',
+    // 处理文件删除状态
+    const handleRemove: UploadProps['onRemove'] = (file: UploadFile) => {
+        return new Promise<boolean>((resolve, reject) => {
+            const url = file?.url ?? '';
+            // 从网址中截取文件的相对路径
+            const idx = url.lastIndexOf('.cn/');
+            const filePath = url.substring(idx + 4, url.length);
+            confirm({
+                centered: true,
+                cancelText: '算了',
+                title: '当真要删除?',
+                icon: <QuestionCircleOutlined />,
+                cancelButtonProps: { shape: 'round' },
+                okButtonProps: { danger: true, shape: 'round' },
+                content: url.match(/\/(\w+\.(?:png|jpg|gif|bmp))$/i)?.[1],
+                async onOk() {
+                    const res = await removeFile({ filePath: filePath });
+                    if (res.success) {
+                        resolve(true);
+                    } else {
+                        reject('Failed');
+                    }
+                },
+                onCancel() {
+                    reject('onCancel');
+                },
+            });
         });
-        break;
-      case undefined:
-        // 这里很重要
-        setFileList([]);
-        break;
-      default:
-        throw new Error('Not implemented yet: undefined case');
-    }
-  };
+    };
 
-  // 处理上传前的文件
-  const handleBeforeUpload = (file: RcFile) => {
-    const UNIT = 1024 * 1024;
-    const curType = file.type;
-    const MAX_FILE_SIZE = fileSize;
-    return new Promise<boolean>((resolve, reject) => {
-      const reader = new FileReader();
-      reader.readAsDataURL(file as Blob);
-      reader.onload = function (e) {
-        const base64: string | ArrayBuffer | null | undefined = e.target?.result;
-        const image = document.createElement('img');
-        image.src = 'string' === typeof base64 ? base64 : (undefined as unknown as string);
-        image.onload = function () {
-          if (!fileType.includes(curType)) {
-            notification.error({
-              message: '文件类型错误',
-              description: `请上传格式为 ${fileType} 的文件`,
-            });
-            reject();
-          } else if (file.size > MAX_FILE_SIZE * UNIT) {
-            notification.error({
-              message: '文件大小不符合要求',
-              description: `单个文件不得超过 ${MAX_FILE_SIZE}M`,
-            });
-            reject();
-          } else if (imageWidth > image.width || imageHeight > image.height) {
-            notification.error({
-              message: '图像尺寸不符合要求',
-              description: `请上传宽高大于或等于 ${imageWidth}X${imageHeight} 的图像`,
-            });
-            reject();
-          } else {
-            resolve(true);
-          }
-        };
-      };
-    });
-  };
+    // 处理上传事件
+    const handleChange: UploadProps['onChange'] = (info: UploadChangeParam) => {
+        const { fileList } = info;
+        setFileList(fileList.slice());
+        const status = info.file.status;
+        switch (status) {
+            case 'uploading':
+                message.info!('File is uploading...');
+                break;
+            case 'done':
+                setFileList([Object.assign({ ...info.file.response.data }, { status: info.file.response.success ? 'done' : 'error' })]);
+                props.setFieldsValue(fileList);
+                message.success!(info.file.response.msg);
+                break;
+            case 'success':
+                message.success!('File uploaded successfully');
+                break;
+            case 'removed':
+                message.success!('File removed successfully');
+                break;
+            case 'error':
+                notification.error({
+                    message: 'Error',
+                    description: info.file?.response?.msg ?? 'File upload failed',
+                });
+                break;
+            case undefined:
+                // 这里很重要
+                setFileList([]);
+                break;
+            default:
+                throw new Error('Not implemented yet: undefined case');
+        }
+    };
 
-  return (
-    <ProForm.Item
-      name={props.formName}
-      label={props?.formLabel}
-      tooltip={props?.formTooltip}
-      rules={props?.validateRules}
-      transform={(value) => props?.useTransForm!(value)}
-    >
-      <ImgCrop grid quality={1} aspect={aspect} modalTitle="裁剪图像">
-        <Upload
-          accept={acceptFile}
-          listType={listType}
-          fileList={fileLists}
-          maxCount={maxUpload}
-          data={props.extraData}
-          onRemove={handleRemove}
-          onChange={handleChange}
-          onPreview={handlePreview}
-          name={props.extraData.field}
-          progress={{
-            strokeColor: {
-              '0%': '#108ee9',
-              '100%': '#87d068',
-            },
-            strokeWidth: 3,
-            showInfo: false,
-          }}
-          className={props?.className}
-          action="/console/public/upload"
-          headers={{ Authorization: localStorage.getItem('Authorization') || '' }}
-          beforeUpload={(file: RcFile) =>
-            handleBeforeUpload(file)
-              .then((res: boolean) => res)
-              .catch(() => false)
-          }
-        >
-          {/*达到限定的上传数量时隐藏上传按钮*/}
-          {maxUpload !== fileLists.length && (
-            <Button type="text" icon={<CloudUploadOutlined />}>
-              {uploadText}
-            </Button>
-          )}
-        </Upload>
-      </ImgCrop>
-    </ProForm.Item>
-  );
+    // 处理上传前的文件
+    const handleBeforeUpload = (file: RcFile) => {
+        const UNIT = 1024 * 1024;
+        const curType = file.type;
+        const MAX_FILE_SIZE = fileSize;
+        return new Promise<boolean>((resolve, reject) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(file as Blob);
+            reader.onload = function (e) {
+                const base64: string | ArrayBuffer | null | undefined = e.target?.result;
+                const image = document.createElement('img');
+                image.src = 'string' === typeof base64 ? base64 : (undefined as unknown as string);
+                image.onload = function () {
+                    if (!fileType.includes(curType)) {
+                        notification.error({
+                            message: '文件类型错误',
+                            description: `请上传格式为 ${fileType} 的文件`,
+                        });
+                        reject();
+                    } else if (file.size > MAX_FILE_SIZE * UNIT) {
+                        notification.error({
+                            message: '文件大小不符合要求',
+                            description: `单个文件不得超过 ${MAX_FILE_SIZE}M`,
+                        });
+                        reject();
+                    } else if (imageWidth > image.width || imageHeight > image.height) {
+                        notification.error({
+                            message: '图像尺寸不符合要求',
+                            description: `请上传宽高大于或等于 ${imageWidth}X${imageHeight} 的图像`,
+                        });
+                        reject();
+                    } else {
+                        resolve(true);
+                    }
+                };
+            };
+        });
+    };
+
+    return (
+        <>
+            <ProForm.Item
+                name={props.formName}
+                label={props?.formLabel}
+                tooltip={props?.formTooltip}
+                rules={props?.validateRules}
+                transform={value => props?.useTransForm!(value)}
+            >
+                <ImgCrop grid quality={1} aspect={aspect} modalTitle="裁剪图像">
+                    <Upload
+                        accept={acceptFile}
+                        listType={listType}
+                        fileList={fileLists}
+                        maxCount={maxUpload}
+                        data={props.extraData}
+                        onRemove={handleRemove}
+                        onChange={handleChange}
+                        name={props.extraData.field}
+                        progress={{
+                            strokeColor: {
+                                '0%': '#108ee9',
+                                '100%': '#87d068',
+                            },
+                            strokeWidth: 3,
+                            showInfo: false,
+                        }}
+                        className={props?.className}
+                        action="/console/public/upload"
+                        onPreview={() => previewRef.current?.imagePreview(true)}
+                        headers={{ Authorization: localStorage.getItem('Authorization') || '' }}
+                        beforeUpload={(file: RcFile) =>
+                            handleBeforeUpload(file)
+                                .then((res: boolean) => res)
+                                .catch(() => false)
+                        }
+                    >
+                        {/*达到限定的上传数量时隐藏上传按钮*/}
+                        {maxUpload !== fileLists.length && (
+                            <Button type="text" icon={<CloudUploadOutlined />}>
+                                {uploadText}
+                            </Button>
+                        )}
+                    </Upload>
+                </ImgCrop>
+            </ProForm.Item>
+            <ImagePreview ref={previewRef} imgList={fileLists} />
+        </>
+    );
 };
--- src/pages/components/UploadButton/index.tsx
+++ src/pages/components/UploadButton/index.tsx
@@ -1,167 +1,171 @@
-import React from 'react';
-import { useModel } from 'umi';
+/** @format */
+
+import { ImagePreview } from '@/pages/components/ImagePreview';
 import { removeFile } from '@/services/ant-design-pro/api';
+import { CloudUploadOutlined, QuestionCircleOutlined } from '@ant-design/icons';
 import { ProFormUploadButton } from '@ant-design/pro-form';
 import { message, Modal, notification, Upload } from 'antd';
-import type { UploadFile, UploadListType } from 'antd/es/upload/interface';
 import type { RcFile, UploadChangeParam, UploadProps } from 'antd/es/upload';
-import { CloudUploadOutlined, QuestionCircleOutlined } from '@ant-design/icons';
+import type { UploadFile, UploadListType } from 'antd/es/upload/interface';
+import React, { useRef } from 'react';
+import { useModel } from 'umi';
 
-export const ProUploadButton: React.FC<API.uploadComponents> = (props) => {
-  const { confirm } = Modal;
-  // 大小限制
-  const fileSize: number = props?.fileSize ?? 2;
-  // 数量限制
-  const maxUpload: number = props?.maxUpload ?? 1;
-  // 图片宽度
-  const imageWidth: number = props?.imageWidth ?? 750;
-  // 图片高度
-  const imageHeight: number = props?.imageHeight ?? 422;
-  // 展示方式
-  const listType: UploadListType = props?.listType ?? 'picture-card';
-  // 文件后缀
-  const acceptFile: string = props?.acceptFile ?? '.png, .jpg, .jpeg, .gif';
-  // 文件列表
-  const { fileLists, setFileLists } = useModel('file', (ret) => ({
-    fileLists: ret.fileList,
-    setFileLists: ret.setFileList,
-  }));
-  // 文件格式
-  const fileType: string[] = props?.fileType ?? ['image/png', 'image/jpeg', 'image/gif'];
-  // 处理上传事件
-  const handleChange: UploadProps['onChange'] = (info: UploadChangeParam) => {
-    const { fileList } = info;
-    setFileLists(fileList.slice());
-    const status = info.file.status;
-    switch (status) {
-      case 'uploading':
-        message.info!('File is uploading...');
-        break;
-      case 'done':
-        message.success!(info.file.response.msg);
-        setFileLists([
-          Object.assign(
-            { ...info.file.response.data },
-            { status: info.file.response.success ? 'done' : 'error' },
-          ),
-        ]);
-        break;
-      case 'success':
-        message.success!('File uploaded successfully');
-        break;
-      case 'removed':
-        message.success!('File removed successfully');
-        break;
-      case 'error':
-        notification.error({
-          message: 'Error',
-          description: info.file?.response?.msg ?? 'File upload failed',
+export const ProUploadButton: React.FC<API.uploadComponents> = props => {
+    const { confirm } = Modal;
+    // 大小限制
+    const fileSize: number = props?.fileSize ?? 2;
+    // 数量限制
+    const maxUpload: number = props?.maxUpload ?? 1;
+    // 图片宽度
+    const imageWidth: number = props?.imageWidth ?? 750;
+    // 图片预览
+    const previewRef: React.ForwardedRef<any> = useRef();
+    // 图片高度
+    const imageHeight: number = props?.imageHeight ?? 422;
+    // 展示方式
+    const listType: UploadListType = props?.listType ?? 'picture-card';
+    // 文件后缀
+    const acceptFile: string = props?.acceptFile ?? '.png, .jpg, .jpeg, .gif';
+    // 文件列表
+    const { fileLists, setFileLists } = useModel('file', ret => ({
+        fileLists: ret.fileList,
+        setFileLists: ret.setFileList,
+    }));
+    // 文件格式
+    const fileType: string[] = props?.fileType ?? ['image/png', 'image/jpeg', 'image/gif'];
+    // 处理上传事件
+    const handleChange: UploadProps['onChange'] = (info: UploadChangeParam) => {
+        const { fileList } = info;
+        setFileLists(fileList.slice());
+        const status = info.file.status;
+        switch (status) {
+            case 'uploading':
+                message.info!('File is uploading...');
+                break;
+            case 'done':
+                message.success!(info.file.response.msg);
+                setFileLists([Object.assign({ ...info.file.response.data }, { status: info.file.response.success ? 'done' : 'error' })]);
+                break;
+            case 'success':
+                message.success!('File uploaded successfully');
+                break;
+            case 'removed':
+                message.success!('File removed successfully');
+                break;
+            case 'error':
+                notification.error({
+                    message: 'Error',
+                    description: info.file?.response?.msg ?? 'File upload failed',
+                });
+                break;
+            default:
+                throw new Error('Not implemented yet: undefined case');
+        }
+    };
+    // 处理文件删除状态
+    const handleRemove: UploadProps['onRemove'] = (file: UploadFile) => {
+        return new Promise<boolean>((resolve, reject) => {
+            const url = file?.url ?? '';
+            // 从网址中截取文件的相对路径
+            const idx = url.lastIndexOf('.cn/');
+            const filePath = url.substring(idx + 4, url.length);
+            confirm({
+                centered: true,
+                cancelText: '算了',
+                title: '当真要删除?',
+                icon: <QuestionCircleOutlined />,
+                cancelButtonProps: { shape: 'round' },
+                okButtonProps: { danger: true, shape: 'round' },
+                content: url.match(/\/(\w+\.(?:png|jpg|gif|bmp))$/i)?.[1],
+                async onOk() {
+                    const res = await removeFile({ filePath: filePath });
+                    if (res.success) {
+                        resolve(true);
+                    } else {
+                        reject('Failed');
+                    }
+                },
+                onCancel() {
+                    reject('onCancel');
+                },
+            });
         });
-        break;
-      default:
-        throw new Error('Not implemented yet: undefined case');
-    }
-  };
-  // 处理文件删除状态
-  const handleRemove: UploadProps['onRemove'] = (file: UploadFile) => {
-    return new Promise<boolean>((resolve, reject) => {
-      const url = file?.url ?? '';
-      // 从网址中截取文件的相对路径
-      const idx = url.lastIndexOf('.cn/');
-      const filePath = url.substring(idx + 4, url.length);
-      confirm({
-        centered: true,
-        cancelText: '算了',
-        title: '当真要删除?',
-        icon: <QuestionCircleOutlined />,
-        cancelButtonProps: { shape: 'round' },
-        okButtonProps: { danger: true, shape: 'round' },
-        content: url.match(/\/(\w+\.(?:png|jpg|gif|bmp))$/i)?.[1],
-        async onOk() {
-          const res = await removeFile({ filePath: filePath });
-          if (res.success) {
-            resolve(true);
-          } else {
-            reject('Failed');
-          }
-        },
-        onCancel() {
-          reject('onCancel');
-        },
-      });
-    });
-  };
-  // 处理上传前的文件
-  const handleBeforeUpload = (file: RcFile) => {
-    const UNIT = 1024 * 1024;
-    const curType = file.type;
-    const MAX_FILE_SIZE = fileSize;
-    return new Promise<boolean>((resolve, reject) => {
-      const reader = new FileReader();
-      reader.readAsDataURL(file as Blob);
-      reader.onload = function (e) {
-        const base64: string | ArrayBuffer | null | undefined = e.target?.result;
-        const image = document.createElement('img');
-        image.src = 'string' === typeof base64 ? base64 : (undefined as unknown as string);
-        image.onload = function () {
-          if (!fileType.includes(curType)) {
-            notification.error({
-              message: '文件类型错误',
-              description: `请上传格式为 ${fileType} 的文件`,
-            });
-            reject();
-          } else if (file.size > MAX_FILE_SIZE * UNIT) {
-            notification.error({
-              message: '文件大小不符合要求',
-              description: `单个文件不得超过 ${MAX_FILE_SIZE}M`,
-            });
-            reject();
-          } else if (imageWidth > image.width || imageHeight > image.height) {
-            notification.error({
-              message: '图像尺寸不符合要求',
-              description: `请上传宽高大于或等于 ${imageWidth}X${imageHeight} 的图像`,
-            });
-            reject();
-          } else {
-            resolve(true);
-          }
-        };
-      };
-    });
-  };
-  return (
-    <ProFormUploadButton
-      max={maxUpload}
-      listType={listType}
-      fileList={fileLists}
-      name={props.formName}
-      label={props?.formLabel}
-      title={props?.formTitle}
-      tooltip={props.formTooltip}
-      rules={props?.validateRules}
-      icon={<CloudUploadOutlined />}
-      action="/console/public/upload"
-      transform={(value) => props?.useTransForm!(value)}
-      fieldProps={{
-        accept: acceptFile,
-        data: props.extraData,
-        onChange: handleChange,
-        onRemove: handleRemove,
-        progress: {
-          strokeColor: {
-            '0%': '#108ee9',
-            '100%': '#87d068',
-          },
-          strokeWidth: 3,
-          showInfo: false,
-        },
-        className: props?.className,
-        beforeUpload: (file: RcFile) =>
-          handleBeforeUpload(file)
-            .then((res) => res)
-            .catch(() => Upload.LIST_IGNORE),
-        headers: { Authorization: localStorage.getItem('Authorization') || '' },
-      }}
-    />
-  );
+    };
+    // 处理上传前的文件
+    const handleBeforeUpload = (file: RcFile) => {
+        const UNIT = 1024 * 1024;
+        const curType = file.type;
+        const MAX_FILE_SIZE = fileSize;
+        return new Promise<boolean>((resolve, reject) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(file as Blob);
+            reader.onload = function (e) {
+                const base64: string | ArrayBuffer | null | undefined = e.target?.result;
+                const image = document.createElement('img');
+                image.src = 'string' === typeof base64 ? base64 : (undefined as unknown as string);
+                image.onload = function () {
+                    if (!fileType.includes(curType)) {
+                        notification.error({
+                            message: '文件类型错误',
+                            description: `请上传格式为 ${fileType} 的文件`,
+                        });
+                        reject();
+                    } else if (file.size > MAX_FILE_SIZE * UNIT) {
+                        notification.error({
+                            message: '文件大小不符合要求',
+                            description: `单个文件不得超过 ${MAX_FILE_SIZE}M`,
+                        });
+                        reject();
+                    } else if (imageWidth > image.width || imageHeight > image.height) {
+                        notification.error({
+                            message: '图像尺寸不符合要求',
+                            description: `请上传宽高大于或等于 ${imageWidth}X${imageHeight} 的图像`,
+                        });
+                        reject();
+                    } else {
+                        resolve(true);
+                    }
+                };
+            };
+        });
+    };
+    return (
+        <>
+            <ProFormUploadButton
+                max={maxUpload}
+                listType={listType}
+                fileList={fileLists}
+                name={props.formName}
+                label={props?.formLabel}
+                title={props?.formTitle}
+                tooltip={props.formTooltip}
+                rules={props?.validateRules}
+                icon={<CloudUploadOutlined />}
+                action="/console/public/upload"
+                transform={value => props?.useTransForm!(value)}
+                fieldProps={{
+                    accept: acceptFile,
+                    data: props.extraData,
+                    onChange: handleChange,
+                    onRemove: handleRemove,
+                    progress: {
+                        strokeColor: {
+                            '0%': '#108ee9',
+                            '100%': '#87d068',
+                        },
+                        strokeWidth: 3,
+                        showInfo: false,
+                    },
+                    className: props?.className,
+                    onPreview: () => previewRef.current?.imagePreview(true),
+                    headers: { Authorization: localStorage.getItem('Authorization') || '' },
+                    beforeUpload: (file: RcFile) =>
+                        handleBeforeUpload(file)
+                            .then(res => res)
+                            .catch(() => Upload.LIST_IGNORE),
+                }}
+            />
+            <ImagePreview ref={previewRef} imgList={fileLists} />
+        </>
+    );
 };
