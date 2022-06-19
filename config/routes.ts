985a51b404336a8f77dfb269db5cf89d70ea4c48
Kevin <wcdma020@hotmail.com>
2022-06-19 22:23:46 +0800

    chore(Kevin): 新增系统设置菜单

--- config/routes.ts
+++ config/routes.ts
@@ -1,116 +1,107 @@
-﻿export default [
-  {
-    path: '/dashboard',
-    name: 'dashboard',
-    icon: 'dashboard',
-    routes: [
-      {
+﻿/** @format */
+
+export default [
+    {
         path: '/dashboard',
-        redirect: '/dashboard/analysis',
-      },
-      {
-        name: 'analysis',
-        icon: 'smile',
-        path: '/dashboard/analysis',
-        component: './dashboard/analysis',
-      },
-      {
-        name: 'monitor',
-        icon: 'smile',
-        path: '/dashboard/monitor',
-        component: './dashboard/monitor',
-      },
-      {
-        name: 'workplace',
-        icon: 'smile',
-        path: '/dashboard/workplace',
-        component: './dashboard/workplace',
-      },
-    ],
-  },
-  {
-    name: 'content',
-    path: '/content',
-    icon: 'align-left',
-    routes: [
-      {
-        name: 'list',
-        path: '/content/list',
-        component: './content/list',
-      },
-      {
-        name: 'edit',
-        path: '/content/edit',
-        component: './content/edit',
-      },
-      {
-        component: '404',
-      },
-    ],
-  },
-  {
-    name: 'channel',
-    path: '/channel',
-    icon: 'unordered-list',
-    component: './channel',
-  },
-  {
-    name: 'user',
-    icon: 'team',
-    path: '/user',
-    routes: [
-      {
-        name: 'login',
-        layout: false,
-        hideInMenu: true,
-        path: '/user/login',
-        component: './user/Login',
-      },
-      {
-        name: 'list',
-        path: '/user/list',
-        component: './user/list',
-      },
-      {
-        name: 'auth-list',
-        path: '/user/authlist',
-        component: './user/auth-list',
-      },
-      {
-        name: 'group-list',
-        path: '/user/grouplist',
-        component: './user/group-list',
-      },
-    ],
-  },
-  {
-    name: 'account',
-    icon: 'user',
-    path: '/account',
-    routes: [
-      {
+        name: 'dashboard',
+        icon: 'dashboard',
+        routes: [
+            {
+                path: '/dashboard',
+                redirect: '/dashboard/analysis',
+            },
+            {
+                name: 'analysis',
+                icon: 'smile',
+                path: '/dashboard/analysis',
+                component: './dashboard/analysis',
+            },
+            {
+                name: 'monitor',
+                icon: 'smile',
+                path: '/dashboard/monitor',
+                component: './dashboard/monitor',
+            },
+            {
+                name: 'workplace',
+                icon: 'smile',
+                path: '/dashboard/workplace',
+                component: './dashboard/workplace',
+            },
+        ],
+    },
+    {
+        name: 'content',
+        path: '/content',
+        icon: 'align-left',
+        routes: [
+            {
+                name: 'list',
+                path: '/content/list',
+                component: './content/list',
+            },
+            {
+                name: 'edit',
+                path: '/content/edit',
+                component: './content/edit',
+            },
+            {
+                component: '404',
+            },
+        ],
+    },
+    {
+        name: 'channel',
+        path: '/channel',
+        icon: 'unordered-list',
+        component: './channel',
+    },
+    {
+        name: 'user',
+        icon: 'team',
+        path: '/user',
+        routes: [
+            {
+                name: 'login',
+                layout: false,
+                hideInMenu: true,
+                path: '/user/login',
+                component: './user/Login',
+            },
+            {
+                name: 'list',
+                path: '/user/list',
+                component: './user/list',
+            },
+            {
+                name: 'auth-list',
+                path: '/user/authlist',
+                component: './user/auth-list',
+            },
+            {
+                name: 'group-list',
+                path: '/user/grouplist',
+                component: './user/group-list',
+            },
+        ],
+    },
+    {
+        name: 'account',
+        icon: 'user',
         path: '/account',
-        redirect: '/account/center',
-      },
-      {
-        name: 'center',
-        icon: 'smile',
-        path: '/account/center',
-        component: './account/center',
-      },
-      {
-        name: 'settings',
-        icon: 'smile',
-        path: '/account/settings',
         component: './account/settings',
-      },
-    ],
-  },
-  {
-    path: '/',
-    redirect: '/dashboard',
-  },
-  {
-    component: './404',
-  },
+    },
+    {
+        name: 'settings',
+        path: '/settings',
+        icon: 'setting',
+        component: './settings',
+    },
+    {
+        path: '/',
+        redirect: '/dashboard',
+    },
+    {
+        component: './404',
+    },
 ];
