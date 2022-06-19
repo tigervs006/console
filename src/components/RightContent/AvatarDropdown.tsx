5ec39d88b1b8e87e000ed6fde4b1bab1239ff41f
Kevin <wcdma020@hotmail.com>
2022-06-19 22:29:13 +0800

    chore(Kevin): 删除个人中心下拉菜单

--- src/components/RightContent/AvatarDropdown.tsx
+++ src/components/RightContent/AvatarDropdown.tsx
@@ -1,3 +1,5 @@
+/** @format */
+
 import styles from './index.less';
 import { history, useModel } from 'umi';
 import { stringify } from 'querystring';
@@ -6,96 +8,85 @@ import React, { useCallback } from 'react';
 import HeaderDropdown from '../HeaderDropdown';
 import type { MenuInfo } from 'rc-menu/lib/interface';
 import { outLogin } from '@/services/ant-design-pro/api';
-import { LogoutOutlined, SettingOutlined, UserOutlined } from '@ant-design/icons';
+import { LogoutOutlined, SettingOutlined } from '@ant-design/icons';
 
 export type GlobalHeaderRightProps = {
-  menu?: boolean;
+    menu?: boolean;
 };
 
 // 菜单项
 const menuItems = [
-  { label: '个人中心', key: 'center', icon: <UserOutlined /> },
-  { label: '个人设置', key: 'settings', icon: <SettingOutlined /> },
-  { label: '退出登录', key: 'logout', icon: <LogoutOutlined /> },
+    { label: '个人设置', key: 'settings', icon: <SettingOutlined /> },
+    { label: '退出登录', key: 'logout', icon: <LogoutOutlined /> },
 ];
 
 /**
  * 退出登录，并且将当前的 url 保存
  */
 const loginOut = async () => {
-  await outLogin({ name: localStorage.getItem('user') || 'anonymous' }).then(() =>
-    localStorage.clear(),
-  );
-  const { query = {}, search, pathname } = history.location;
-  const { redirect } = query;
-  // Note: There may be security issues, please note
-  if (window.location.pathname !== '/user/login' && !redirect) {
-    history.replace({
-      pathname: '/user/login',
-      search: stringify({
-        redirect: pathname + search,
-      }),
-    });
-  }
+    await outLogin({ name: localStorage.getItem('user') || 'anonymous' }).then(() => localStorage.clear());
+    const { query = {}, search, pathname } = history.location;
+    const { redirect } = query;
+    // Note: There may be security issues, please note
+    if (window.location.pathname !== '/user/login' && !redirect) {
+        history.replace({
+            pathname: '/user/login',
+            search: stringify({
+                redirect: pathname + search,
+            }),
+        });
+    }
 };
 
 const AvatarDropdown: React.FC<GlobalHeaderRightProps> = () => {
-  const { initialState, setInitialState } = useModel('@@initialState');
+    const { initialState, setInitialState } = useModel('@@initialState');
 
-  const onMenuClick = useCallback(
-    (event: MenuInfo) => {
-      const { key } = event;
-      switch (key) {
-        case 'logout':
-          setInitialState((s) => ({ ...s, currentUser: undefined }));
-          loginOut();
-          break;
-        case 'center':
-          history.push(`/account/${key}`);
-          break;
-        case 'settings':
-          history.push(`/account/${key}`);
-          break;
-        default:
-          return;
-      }
-    },
-    [setInitialState],
-  );
+    const onMenuClick = useCallback(
+        (event: MenuInfo) => {
+            const { key } = event;
+            switch (key) {
+                case 'logout':
+                    setInitialState!(s => ({ ...s, currentUser: undefined }));
+                    loginOut!();
+                    break;
+                default:
+                    history.push(`/account/${key}`);
+            }
+        },
+        [setInitialState],
+    );
 
-  const loading = (
-    <span className={`${styles.action} ${styles.account}`}>
-      <Spin
-        size="small"
-        style={{
-          marginLeft: 8,
-          marginRight: 8,
-        }}
-      />
-    </span>
-  );
+    const loading = (
+        <span className={`${styles.action} ${styles.account}`}>
+            <Spin
+                size="small"
+                style={{
+                    marginLeft: 8,
+                    marginRight: 8,
+                }}
+            />
+        </span>
+    );
 
-  if (!initialState) {
-    return loading;
-  }
+    if (!initialState) {
+        return loading;
+    }
 
-  const { currentUser } = initialState;
+    const { currentUser } = initialState;
 
-  if (!currentUser || !currentUser.name) {
-    return loading;
-  }
+    if (!currentUser || !currentUser.name) {
+        return loading;
+    }
 
-  const menuHeaderDropdown = (
-    <Menu className={styles.menu} selectedKeys={[]} onClick={onMenuClick} items={menuItems} />
-  );
-  return (
-    <HeaderDropdown overlay={menuHeaderDropdown}>
-      <span className={`${styles.action} ${styles.account}`}>
-        <Avatar size="small" className={styles.avatar} src={currentUser.avatar} alt="avatar" />
-        <span className={`${styles.name} anticon`}>{currentUser.name}</span>
-      </span>
-    </HeaderDropdown>
-  );
+    const menuHeaderDropdown = <Menu className={styles.menu} selectedKeys={[]} onClick={onMenuClick} items={menuItems} />;
+    return (
+        <HeaderDropdown overlay={menuHeaderDropdown}>
+            <span className={`${styles.action} ${styles.account}`}>
+                <Avatar size="small" className={styles.avatar} src={currentUser.avatar} alt="avatar" />
+                <span className={`${styles.name} anticon`}>{currentUser.name}</span>
+            </span>
+        </HeaderDropdown>
+    );
 };
 
 export default AvatarDropdown;
