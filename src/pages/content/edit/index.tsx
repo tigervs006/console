107c162553197b93316ef794921cc5d4bc9f3faf
Kevin <wcdma020@hotmail.com>
2022-06-25 01:26:50 +0800

    feat(Kevin): 调整异步请求返回的结果处理逻辑

--- src/pages/content/edit/index.tsx
+++ src/pages/content/edit/index.tsx
@@ -15,195 +15,202 @@ import { getChannel, saveContent, getContent as getContents } from '@/pages/cont
 import ProForm, { ProFormDependency, ProFormCheckbox, ProFormTextArea, ProFormSelect, ProFormText } from '@ant-design/pro-form';
 
 export default () => {
-    const formRef = useRef<ProFormInstance>();
-    // 文档内容
-    const [content, setContent] = useState<string>(() => {
-        return formRef.current?.getFieldValue('content') || null;
-    });
-    // 文件列表
-    const { setFileLists } = useModel('file', ret => ({
-        setFileLists: ret.setFileList,
-    }));
-    // 新闻栏目
-    const channel = async () => {
-        const res = await getChannel();
-        return (
-            res?.data?.list.map((item: channelDataItem) => ({
-                value: item.id,
-                label: item.cname,
-            })) ?? {}
-        );
-    };
-    // 提取图像
-    const adstractImg = () => {
-        // 从正文提取图像
-        if (null === content) {
-            return notification.error({
-                message: '正文内容为空',
-                description: '请先完善正文内容并插入图像',
-            });
-        }
-        const imgArr = extractImg(content);
-        if (imgArr.length) {
-            notification.success({
-                message: '提取图像成功',
-                description: imgArr.toString().match(/\/(\w+\.(?:png|jpg|gif|bmp))$/i)?.[1],
-            });
-        } else {
-            notification.error({
-                message: '提取图像失败',
-                description: '正文中不包含图像',
-            });
-        }
-        formRef.current?.setFieldsValue({ litpic: imgArr.toString() });
-    };
-    // 获取编辑器内容
-    const getContent = (CKcontent: string) => {
-        // 设置useState
-        setContent(CKcontent);
-        // 设置到字段中只是为了rules规则验证
-        formRef.current?.setFieldsValue({ content: CKcontent });
-    };
+	const formRef = useRef<ProFormInstance>();
+	// 文档内容
+	const [content, setContent] = useState<string>(() => {
+		return formRef.current?.getFieldValue('content') || null;
+	});
+	// 文件列表
+	const {setFileLists} = useModel('file', ret => ({
+		setFileLists: ret.setFileList,
+	}));
+	// 新闻栏目
+	const channel = async () => {
+		return await getChannel().then(res => (
+			res.data?.list.map((item: channelDataItem) => (
+				{
+					value: item.id,
+					label: item.cname,
+				}
+			))
+		));
+	};
+	// 提取图像
+	const adstractImg = () => {
+		// 从正文提取图像
+		if (null === content) {
+			return notification.error({
+				message: '正文内容为空',
+				description: '请先完善正文内容并插入图像',
+			});
+		}
+		const imgArr = extractImg(content);
+		if (imgArr.length) {
+			notification.success({
+				message: '提取图像成功',
+				description: imgArr.toString().match(/\/(\w+\.(?:png|jpg|gif|bmp))$/i)?.[1],
+			});
+		} else {
+			notification.error({
+				message: '提取图像失败',
+				description: '正文中不包含图像',
+			});
+		}
+		formRef.current?.setFieldsValue({litpic: imgArr.toString()});
+	};
+	// 获取编辑器内容
+	const getContent = (CKcontent: string) => {
+		// 设置useState
+		setContent(CKcontent);
+		// 设置到字段中只是为了rules规则验证
+		formRef.current?.setFieldsValue({content: CKcontent});
+	};
 
-    // 处理onFinsh提交
-    const handleFinsh = async (data: articleData) => {
-        await saveContent(
-            Object.assign(
-                { ...data },
-                {
-                    content: content,
-                    id: history.location.query?.id ?? null,
-                    author: localStorage?.getItem('user') ?? 'anonymous',
-                },
-            ),
-        ).then(res => {
-            message.success(res.msg);
-            // 延时跳转到列表页
-            waitTime(2500).then(() => history.push({ pathname: '/content/list' }));
-        });
-    };
+	// 处理onFinsh提交
+	const handleFinsh = async (data: articleData) => {
+		await saveContent(
+			Object.assign(
+				{...data},
+				{
+					content: content,
+					id: history.location.query?.id ?? null,
+					author: localStorage?.getItem('user') ?? 'anonymous',
+				},
+			),
+		).then(res => {
+			message.success(res.msg);
+			// 延时跳转到列表页
+			waitTime(2000).then(() => history.push({pathname: '/content/list'}));
+		});
+	};
 
-    // 处理request请求
-    const handleRequest = async (params: Record<'id', string>) => {
-        if (params?.id) {
-            // 只有在编辑文档时请求网络
-            const res = await getContents({ ...params });
-            const info = res?.data?.info ?? {};
-            // 设置编辑器内容
-            setContent(info?.content?.content ?? null);
-            // 设置fileList
-            setFileLists([
-                {
-                    status: 'done',
-                    url: info.litpic,
-                    uid: Math.floor(Math.random() * 100).toString(),
-                    name: info.litpic.match(/\/(\w+\.(?:png|jpg|gif|bmp))$/i)[1],
-                },
-            ]);
-            const attribute = [];
-            // 提取值为1的属性名
-            for (const idx in info) {
-                if ((idx === 'is_head' && 1 === info[idx]) || (idx === 'is_recom' && 1 === info[idx]) || (idx === 'is_litpic' && 1 === info[idx])) {
-                    attribute.push(idx);
-                }
-            }
-            return res?.data ? Object.assign({ ...info }, { attribute: attribute || [], content: info?.content?.content ?? null }) : {};
-        } else {
-            // 清空fileList
-            setFileLists([]);
-            return {}; // 不是编辑文档直接返回空对象
-        }
-    };
+	// 处理request请求
+	const handleRequest = async (params: Record<'id', string>) => {
+		if (params?.id) {
+			// 只有在编辑文档时请求网络
+			return await getContents({...params}).then(res => {
+				const info = res?.data?.info ?? {};
+				// 设置编辑器内容
+				setContent(info?.content?.content ?? null);
+				// 设置fileList
+				setFileLists([
+					{
+						status: 'done',
+						url: info.litpic,
+						uid: Math.floor(Math.random() * 100).toString(),
+						name: info.litpic.match(/\/(\w+\.(?:png|jpg|gif|bmp))$/i)[1],
+					},
+				]);
+				const attribute = [];
+				// 提取值为1的属性名
+				for (const idx in info) {
+					if ((idx === 'is_head' && 1 === info[idx])
+						|| (idx === 'is_recom' && 1 === info[idx])
+						|| (idx === 'is_litpic' && 1 === info[idx])) {
+						attribute.push(idx);
+					}
+				}
+				return res?.data ? Object.assign({...info}, {
+					attribute: attribute || [],
+					content: info?.content?.content ?? null
+				}) : {};
+			})
+		} else {
+			// 清空fileList
+			setFileLists([]);
+			return {}; // 不是编辑文档直接返回空对象
+		}
+	};
 
-    return (
-        <PageContainer>
-            <ProForm<articleData>
-                formRef={formRef}
-                layout="vertical"
-                wrapperCol={{
-                    md: { span: 16 },
-                    lg: { span: 16 },
-                    xl: { span: 8 },
-                }}
-                // 表单默认值
-                initialValues={{
-                    cid: 4,
-                    attribute: ['is_recom'],
-                }}
-                submitter={{
-                    render: (_, doms) => {
-                        return (
-                            <Space size="middle" key="spaceGroup">
-                                {doms}
-                            </Space>
-                        );
-                    },
-                    resetButtonProps: { shape: 'round', icon: <UndoOutlined /> },
-                    submitButtonProps: { type: 'primary', shape: 'round', icon: <FormOutlined /> },
-                }}
-                // 失焦校验数据
-                validateTrigger={['onBlur']}
-                // request参数
-                params={{ id: history.location.query?.id }}
-                // 提交文档数据
-                onFinish={(data: articleData) => handleFinsh(data)}
-                // request请求
-                request={(params: Record<'id', string>) => handleRequest(params)}
-            >
-                <ProFormSelect
-                    hasFeedback
-                    width="xs"
-                    name="cid"
-                    label="栏目"
-                    request={channel}
-                    tooltip="文档发布的栏目"
-                    fieldProps={{ allowClear: false }}
-                    rules={[{ required: true, message: '选择文档发布的栏目' }]}
-                />
-                <ProFormText
-                    hasFeedback
-                    label="标题"
-                    name="title"
-                    tooltip="限制32个字符"
-                    placeholder="请输入文档标题"
-                    // 输入时去除首尾空格
-                    getValueFromEvent={e => e.target.value.trim()}
-                    fieldProps={{ maxLength: 32, showCount: true }}
-                    rules={[
-                        { required: true, message: '请输入文档标题' },
-                        { min: 15, message: '文档标题不宜太短' },
-                    ]}
-                />
-                <ProFormText
-                    hasFeedback
-                    label="关键词"
-                    name="keywords"
-                    tooltip="请用空格分隔"
-                    placeholder="请输入关键词"
-                    fieldProps={{ showCount: true, maxLength: 64 }}
-                    rules={[
-                        { required: true, message: '请输入关键词' },
-                        { min: 10, message: '再多来两个关键词' },
-                        {
-                            type: 'string',
-                            pattern: /^[^\u2018-\u2027\uff01-\uff0f\uff1a-\uff20\u3002]+$/,
-                            message: '关键词只能使用英文逗号或空格分隔',
-                        },
-                    ]}
-                />
-                <ProFormTextArea
-                    hasFeedback
-                    label="文档简述"
-                    name="description"
-                    tooltip="SEO优化很重要"
-                    placeholder="请输入名称文档简述"
-                    // 输入时去除首尾空格
-                    getValueFromEvent={e => e.target.value.trim()}
-                    fieldProps={{
-                        allowClear: true,
-                        showCount: true,
-                        maxLength: 256,
-                        autoSize: { minRows: 5, maxRows: 8 },
+	return (
+		<PageContainer>
+			<ProForm<articleData>
+				formRef={formRef}
+				layout="vertical"
+				wrapperCol={{
+					md: {span: 16},
+					lg: {span: 16},
+					xl: {span: 8},
+				}}
+				// 表单默认值
+				initialValues={{
+					cid: 4,
+					attribute: ['is_recom'],
+				}}
+				submitter={{
+					render: (_, doms) => {
+						return (
+							<Space size="middle" key="spaceGroup">
+								{doms}
+							</Space>
+						);
+					},
+					resetButtonProps: {shape: 'round', icon: <UndoOutlined/>},
+					submitButtonProps: {type: 'primary', shape: 'round', icon: <FormOutlined/>},
+				}}
+				// 失焦校验数据
+				validateTrigger={['onBlur']}
+				// request参数
+				params={{id: history.location.query?.id}}
+				// 提交文档数据
+				onFinish={(data: articleData) => handleFinsh(data)}
+				// request请求
+				request={(params: Record<'id', string>) => handleRequest(params)}
+			>
+				<ProFormSelect
+					hasFeedback
+					width="xs"
+					name="cid"
+					label="栏目"
+					request={channel}
+					tooltip="文档发布的栏目"
+					fieldProps={{allowClear: false}}
+					rules={[{required: true, message: '选择文档发布的栏目'}]}
+				/>
+				<ProFormText
+					hasFeedback
+					label="标题"
+					name="title"
+					tooltip="限制32个字符"
+					placeholder="请输入文档标题"
+					// 输入时去除首尾空格
+					getValueFromEvent={e => e.target.value.trim()}
+					fieldProps={{maxLength: 32, showCount: true}}
+					rules={[
+						{required: true, message: '请输入文档标题'},
+						{min: 15, message: '文档标题不宜太短'},
+					]}
+				/>
+				<ProFormText
+					hasFeedback
+					label="关键词"
+					name="keywords"
+					tooltip="请用空格分隔"
+					placeholder="请输入关键词"
+					fieldProps={{showCount: true, maxLength: 64}}
+					rules={[
+						{required: true, message: '请输入关键词'},
+						{min: 10, message: '再多来两个关键词'},
+						{
+							type: 'string',
+							pattern: /^[^\u2018-\u2027\uff01-\uff0f\uff1a-\uff20\u3002]+$/,
+							message: '关键词只能使用英文逗号或空格分隔',
+						},
+					]}
+				/>
+				<ProFormTextArea
+					hasFeedback
+					label="文档简述"
+					name="description"
+					tooltip="SEO优化很重要"
+					placeholder="请输入名称文档简述"
+					// 输入时去除首尾空格
+					getValueFromEvent={e => e.target.value.trim()}
+					fieldProps={{
+						allowClear: true,
+						showCount: true,
+						maxLength: 256,
+						autoSize: {minRows: 5, maxRows: 8},
                     }}
                     rules={[
                         { required: true, message: '请输入文档简述' },
