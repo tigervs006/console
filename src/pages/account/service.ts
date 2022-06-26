054b522a60eabfddcce8e58a573a4dbb50c7a84a
Kevin <wcdma020@hotmail.com>
2022-06-26 17:11:42 +0800

    chore(Kevin): 调整文件及文件夹路径

--- src/pages/account/service.ts
+++ src/pages/account/service.ts
@@ -1,14 +1,16 @@
+/** @format */
+
 import { request } from 'umi';
 import type { CurrentUser, GeographicItemType } from './data';
 
 export async function queryCurrent(): Promise<{ data: CurrentUser }> {
-  return request('/account/settings');
+    return request('/account/settings');
 }
 
 export async function queryProvince(): Promise<{ data: GeographicItemType[] }> {
-  return request('/account/province');
+    return request('/account/province');
 }
 
 export async function queryCity(province: string): Promise<{ data: GeographicItemType[] }> {
-  return request(`/account/city?code=${province}`);
+    return request(`/account/city?code=${province}`);
 }
