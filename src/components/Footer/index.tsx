20333fdb23356fabf74e897b76214e67102bde71
Kevin <wcdma020@hotmail.com>
2022-06-27 08:45:19 +0800

    chore(Kevin): 调整默认footer信息

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,41 +1,42 @@
+/** @format */
+
+import React from 'react';
 import { useIntl } from 'umi';
-import { GithubOutlined } from '@ant-design/icons';
 import { DefaultFooter } from '@ant-design/pro-layout';
 
 const Footer: React.FC = () => {
-  const intl = useIntl();
-  const defaultMessage = intl.formatMessage({
-    id: 'app.copyright.produced',
-    defaultMessage: '蚂蚁集团体验技术部出品',
-  });
+    const intl = useIntl();
+    const defaultMessage = intl.formatMessage({
+        id: 'app.copyright.produced',
+        defaultMessage: '北蓝得（深圳）科技有限公司 Copyright ©2022 All Rights Reserved',
+    });
 
-  const currentYear = new Date().getFullYear();
+    const currentYear = new Date().getFullYear();
 
-  return (
-    <DefaultFooter
-      copyright={`${currentYear} ${defaultMessage}`}
-      links={[
-        {
-          key: 'Ant Design Pro',
-          title: 'Ant Design Pro',
-          href: 'https://pro.ant.design',
-          blankTarget: true,
-        },
-        {
-          key: 'github',
-          title: <GithubOutlined />,
-          href: 'https://github.com/ant-design/ant-design-pro',
-          blankTarget: true,
-        },
-        {
-          key: 'Ant Design',
-          title: 'Ant Design',
-          href: 'https://ant.design',
-          blankTarget: true,
-        },
-      ]}
-    />
-  );
+    return (
+        <DefaultFooter
+            copyright={`${currentYear} ${defaultMessage}`}
+            links={[
+                {
+                    key: '田者高拍官网',
+                    title: '田者高拍',
+                    href: 'https://www.brandsz.cn',
+                    blankTarget: true,
+                },
+                {
+                    key: '|',
+                    title: '|',
+                    href: 'javascrip:',
+                },
+                {
+                    key: '如虎科技官网',
+                    title: '如虎科技',
+                    href: 'https://www.tigervs.com',
+                    blankTarget: true,
+                },
+            ]}
+        />
+    );
 };
 
 export default Footer;
