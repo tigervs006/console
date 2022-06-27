4efcc7accb87a3c2fcb5defcd97c9c12632d07ef
Kevin <wcdma020@hotmail.com>
2022-06-27 08:48:24 +0800

    chore(Kevin): 新增通过举枚方式渲染从服务器获取到的菜单icon

---
+++ src/extra/menu.tsx
@@ -0,0 +1,29 @@
+/** @format */
+
+import type { MenuDataItem } from '@ant-design/pro-layout';
+
+import {
+    UnorderedListOutlined,
+    AlignLeftOutlined,
+    DashboardOutlined,
+    UserSwitchOutlined,
+    SettingOutlined,
+    TeamOutlined,
+    UserOutlined,
+} from '@ant-design/icons';
+
+const IconMap = {
+    team: <TeamOutlined />,
+    user: <UserOutlined />,
+    setting: <SettingOutlined />,
+    dashboard: <DashboardOutlined />,
+    'align-left': <AlignLeftOutlined />,
+    'user-switch': <UserSwitchOutlined />,
+    'unordered-list': <UnorderedListOutlined />,
+};
+export const loopMenuItem = (menus: MenuDataItem[]): MenuDataItem[] =>
+    menus.map(({ icon, routes, ...item }) => ({
+        ...item,
+        icon: icon && IconMap[icon as string],
+        routes: routes && loopMenuItem(routes),
+    }));
