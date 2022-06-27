f30240a031b66a73b2ded81b71c3071a81f27274
Kevin <wcdma020@hotmail.com>
2022-06-27 08:37:54 +0800

    chore(Kevin): 新增currentUserMenu接口

--- src/services/ant-design-pro/api.ts
+++ src/services/ant-design-pro/api.ts
@@ -1,49 +1,58 @@
+/** @format */
+
 import { request } from 'umi';
 import { stringify } from 'qs';
 
 /** 获取当前的用户 GET /user */
 export async function currentUser(params: { id: string }, options?: Record<string, any>) {
-  return request<{
-    data: API.CurrentUser;
-  }>('/user', {
-    method: 'GET',
-    params: { ...params },
-    ...(options || {}),
-  });
+    return request<{ data: API.CurrentUser }>('/user', {
+        method: 'GET',
+        params: { ...params },
+        ...(options || {}),
+    });
+}
+
+/** 获取当前用户菜单 GET /auth/list */
+export async function currentUserMenu(params: Record<string, any>, options?: Record<string, any>) {
+    return request<Record<string, any>>('/auth/list', {
+        method: 'GET',
+        params: { ...params },
+        ...(options || {}),
+    });
 }
 
 /** 退出登录接口 POST /public/logout */
 export async function outLogin(data: { name: string }, options?: Record<string, any>) {
-  return request<Record<string, any>>('/public/logout', {
-    method: 'POST',
-    data: { ...data },
-    ...(options || {}),
-  });
+    return request<Record<string, any>>('/public/logout', {
+        method: 'POST',
+        data: { ...data },
+        ...(options || {}),
+    });
 }
 
 /** 登录接口 POST /public/login */
 export async function login(body: API.LoginParams, options?: Record<string, any>) {
-  return request<API.LoginResult>('/public/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    data: body,
-    ...(options || {}),
-  });
+    return request<API.LoginResult>('/public/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        data: body,
+        ...(options || {}),
+    });
 }
 
 /** 此处后端没有提供注释 GET /api/notices */
 export async function getNotices(options?: Record<string, any>) {
-  return request<API.NoticeIconList>('/api/notices', {
-    method: 'GET',
-    ...(options || {}),
-  });
+    return request<API.NoticeIconList>('/api/notices', {
+        method: 'GET',
+        ...(options || {}),
+    });
 }
 
 /** 文件删除 POST /public/remove */
 export async function removeFile(data: { filePath: string }) {
-  return postData('/public/remove', { ...data });
+    return postData('/public/remove', { ...data });
 }
 
 /**
@@ -52,17 +61,13 @@ export async function removeFile(data: { filePath: string }) {
  * @param params 参数
  * @param options options
  */
-export async function getData(
-  path: string,
-  params?: Record<string, any>,
-  options?: Record<string, any>,
-) {
-  return request<Record<string, any>>(path, {
-    method: 'GET',
-    params: { ...params },
-    ...(options || {}),
-    paramsSerializer: () => stringify(params, { arrayFormat: 'indices' }),
-  });
+export async function getData(path: string, params?: Record<string, any>, options?: Record<string, any>) {
+    return request<Record<string, any>>(path, {
+        method: 'GET',
+        params: { ...params },
+        ...(options || {}),
+        paramsSerializer: () => stringify(params, { arrayFormat: 'indices' }),
+    });
 }
 
 /**
@@ -71,14 +76,10 @@ export async function getData(
  * @param data 数据
  * @param options options
  */
-export async function postData(
-  path: string,
-  data: Record<string, any>,
-  options?: Record<string, any>,
-) {
-  return request<Record<string, any>>(path, {
-    method: 'POST',
-    data: { ...data },
-    ...(options || {}),
-  });
+export async function postData(path: string, data: Record<string, any>, options?: Record<string, any>) {
+    return request<Record<string, any>>(path, {
+        method: 'POST',
+        data: { ...data },
+        ...(options || {}),
+    });
 }
