4d634685e9743b2570f7ed837925db8215ecbedd
Kevin <wcdma020@hotmail.com>
2022-06-30 23:10:10 +0800

    chore(Kevin): 新增menuDataItem参数类型

--- src/pages/user/data.d.ts
+++ src/pages/user/data.d.ts
@@ -1,12 +1,30 @@
+/** @format */
+
 export type tableDataItem = {
-  id?: number;
-  gid?: number;
-  name?: string;
-  cname?: string;
-  status?: number;
-  email?: string;
-  avatar?: string;
-  ipaddress?: number;
-  last_login?: string;
-  create_time?: string;
+    id?: number;
+    gid?: number;
+    name?: string;
+    cname?: string;
+    status?: number;
+    email?: string;
+    avatar?: string;
+    ipaddress?: number;
+    last_login?: string;
+    create_time?: string;
+};
+
+export type menuDataItem = {
+    pid?: number;
+    name?: string;
+    icon?: string;
+    path?: string;
+    exact?: number;
+    status?: number;
+    locale?: string;
+    plocale?: string;
+    hideInMenu?: number;
+    id?: number | string;
+    authority?: string[];
+    children?: menuDataItem[];
+    hideChildrenInMenu?: number;
 };
