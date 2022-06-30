951a0393438eeecc5a6e42fa938e177e8710e85d
Kevin <wcdma020@hotmail.com>
2022-06-30 23:11:44 +0800

    chore(Kevin): 新增saveMenu、fetchMenuData接口

--- src/pages/user/service.ts
+++ src/pages/user/service.ts
@@ -1,17 +1,29 @@
-import type { tableDataItem } from '@/pages/user/data';
+/** @format */
+
+import type { menuDataItem, tableDataItem } from '@/pages/user/data';
 import { getData, postData } from '@/services/ant-design-pro/api';
 
 // 获取用户列表
 export async function fetchData(params: API.PageParams) {
-  return getData('/user/list', { ...params });
+    return getData('/user/list', { ...params });
 }
 
 // 新增/编辑用户
 export async function saveUser(data: tableDataItem) {
-  return postData('/user/save', { ...data });
+    return postData('/user/save', { ...data });
 }
 
 // 单个/批量删除
 export async function remove(data: Record<'id', number | number[]>) {
-  return postData('/user/del', { ...data });
+    return postData('/user/del', { ...data });
+}
+
+/* 新增/编辑菜单 */
+export async function saveMenu(data: menuDataItem) {
+    return postData('/auth/save', { ...data });
+}
+
+/* 获取菜单列表 */
+export async function fetchMenuData(params: Record<string, any>) {
+    return getData('/auth/list', { ...params });
 }
