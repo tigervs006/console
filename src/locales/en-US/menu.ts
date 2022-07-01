45d0f9b9b681c3c87c21e7b19ff6b218a3344700
Kevin <wcdma020@hotmail.com>
2022-07-01 08:45:19 +0800

    chore(Kevin): 调整多用户菜单多语言

--- src/locales/en-US/menu.ts
+++ src/locales/en-US/menu.ts
@@ -61,7 +61,7 @@ export default {
     'menu.user': 'User',
     'menu.user.login': 'UserLogin',
     'menu.user.list': 'UserList',
-    'menu.user.auth': 'AuthMenu',
+    'menu.user.menu': 'UserMenu',
     'menu.user.group': 'AuthGroup',
     'menu.top': 'Top Menu',
 };
--- src/locales/zh-CN/menu.ts
+++ src/locales/zh-CN/menu.ts
@@ -61,7 +61,7 @@ export default {
     'menu.user': '用户管理',
     'menu.user.login': '用户登录',
     'menu.user.list': '用户列表',
-    'menu.user.auth': '权限菜单',
+    'menu.user.menu': '用户菜单',
     'menu.user.group': '用户组列表',
     'menu.top': '顶级菜单',
 };
