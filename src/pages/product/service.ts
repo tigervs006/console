cd39d07a542e68da7a46af85bb74b34bce67030b
Kevin <wcdma020@hotmail.com>
2022-07-09 01:09:04 +0800

    chore(Kevin): 使用箭头函数重构api访问接口方法

--- src/pages/product/service.ts
+++ src/pages/product/service.ts
@@ -1,23 +1,16 @@
 /** @format */
 
-import { productDataItem } from '@/pages/product/data';
+import type { productDataItem } from '@/pages/product/data';
 import { getData, postData } from '@/services/ant-design-pro/api';
 
 /* 获取商品分类 */
-export async function getCate() {
-    return getData('/product/cate');
-}
+export const getCate = async () => getData('/product/cate');
+
 /* 获取商品信息 */
-export async function getInfo(params: { id?: string }) {
-    return getData('/product/list', { ...params });
-}
+export const getInfo = async (params: { id?: string }) => getData('/product/list', params);
 
 /* 新增/编辑商品 */
-export async function saveProduct(data: productDataItem) {
-    return postData('/product/save', { ...data });
-}
+export const saveProduct = async (data: productDataItem) => postData('/product/save', data);
 
 /* 单个/批量删除 */
-export async function remove(data: { id: number | number[] }) {
-    return postData('/product/del', { ...data });
-}
+export const remove = async (data: { id: number | number[] }) => postData('/product/del', data);
