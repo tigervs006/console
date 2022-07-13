cce27883e8cd596ba9c37902455810b12c840855
Kevin <wcdma020@hotmail.com>
2022-07-13 09:40:01 +0800

    chore(Kevin): 新增remove接口，调整getInfo接口地址

--- src/pages/product/service.ts
+++ src/pages/product/service.ts
@@ -7,10 +7,13 @@ import { getData, postData } from '@/services/ant-design-pro/api';
 export const getCate = () => getData('/product/cate');
 
 /* 获取商品信息 */
-export const getInfo = (params: { id?: string }) => getData('/product/list', params);
+export const getInfo = (params: { id?: string }) => getData('/product', params);
 
 /* 新增/编辑商品 */
 export const saveProduct = (data: productDataItem) => postData('/product/save', data);
 
 /* 单个/批量删除 */
-export const remove = (data: { id: number | number[] }) => postData('/product/del', data);
+export const remove = (data: { id: string | string[] }) => postData('/product/del', data);
+
+/* 获取商品列表 */
+export const fetchData = (params: Record<string, any>) => getData('/product/list', params);
