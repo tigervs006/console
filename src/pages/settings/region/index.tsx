74120172465fcb4fa8dd8d6d305a4e17e80266d5
Kevin <wcdma020@hotmail.com>
2022-07-15 13:25:56 +0800

    chore(Kevin): 新增行政区域配置页面

---
+++ src/pages/settings/region/index.tsx
@@ -0,0 +1,11 @@
+/** @format */
+
+import { PageContainer } from '@ant-design/pro-layout';
+
+export default () => {
+    return (
+        <PageContainer>
+            <b>行政区域</b>
+        </PageContainer>
+    );
+};
