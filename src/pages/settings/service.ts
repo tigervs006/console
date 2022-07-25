2917f791ec225598c63077d975d034c117670dcd
Kevin <wcdma020@hotmail.com>
2022-07-25 16:23:50 +0800

    chore(Kevin): 新增行政区域若干接口

--- src/pages/settings/service.ts
+++ src/pages/settings/service.ts
@@ -1,9 +1,18 @@
 /** @format */
-
+import type { regionDataItem } from './data';
 import { getData, postData } from '@/services/ant-design-pro/api';
 
 /** 获取配置列表 */
 export const list = (param: { type: string }) => getData('/system/list', param);
 
+/** 新增/编辑地区 */
+export const saveRegion = (data: regionDataItem) => postData('/region/save', data);
+
 /** 更新系统配置 */
 export const saveConfig = (data: Record<string, any>) => postData('/system/save', data);
+
+/** 单个/批量删除 */
+export const remove = (data: Record<'id', number | number[]>) => postData('/region/del', data);
+
+/** 获取地区列表 */
+export const fetchRegionData = (params: Record<string, any>) => getData('/region/list', params);
