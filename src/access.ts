68121a75fdee036a8720f137dc553beee0e52fdd
Kevin <wcdma020@hotmail.com>
2022-07-31 01:40:44 +0800

    chore(Kevin): 调整鉴权参数

--- src/access.ts
+++ src/access.ts
@@ -1,9 +1,11 @@
 /**
+ * @format
  * @see https://umijs.org/zh-CN/plugins/plugin-access
- * */
+ */
+
 export default function access(initialState: { currentUser?: API.CurrentUser } | undefined) {
-  const { currentUser } = initialState ?? {};
-  return {
-    canAdmin: currentUser && currentUser.access === 'admin',
-  };
+    const { currentUser } = initialState ?? {};
+    return {
+        canAdmin: currentUser && currentUser.isAdmin,
+    };
 }
