562bb48e2827f3850062929de788b51248f6c35a
Kevin <wcdma020@hotmail.com>
2022-08-18 09:10:33 +0800

    chore(Kevin): 新增路由鉴权功能

--- src/access.ts
+++ src/access.ts
@@ -2,10 +2,36 @@
  * @format
  * @see https://umijs.org/zh-CN/plugins/plugin-access
  */
+import { history } from 'umi';
+import type { MenuDataItem } from '@ant-design/pro-layout';
 
-export default function access(initialState: { currentUser?: API.CurrentUser } | undefined) {
-    const { currentUser } = initialState ?? {};
+/**
+ * 检查权限
+ * @param key key
+ * @param extra 参数
+ * @param menuData 用户菜单
+ */
+const checkPermission = (extra: string, key: string, menuData?: MenuDataItem[]) => {
+    /* 扁平化菜单 */
+    const filterData = (menuDatas: MenuDataItem[], init: string[] = []) => {
+        menuDatas?.filter(item => {
+            init.push(item[key]);
+            item?.children && filterData(item.children, init);
+        });
+        return init;
+    };
+    return filterData(menuData!).includes(extra);
+};
+
+export default function (initialState: { currentUser?: API.CurrentUser; userMenuItem?: MenuDataItem[] }) {
+    const { userMenuItem } = initialState ?? {};
     return {
-        canAdmin: currentUser && currentUser.isAdmin,
+        /* 按钮权限 */
+        actionFilter: (value: string) => {
+            console.log('actionFilter', value);
+            return false;
+        },
+        /* 路由权限 */
+        authFilter: () => checkPermission(history.location.pathname, 'path', userMenuItem),
     };
 }
