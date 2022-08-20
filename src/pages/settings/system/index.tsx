62ae05a9c6bb2e2aaceb3bdcfc894c288f27402a
Kevin <wcdma020@hotmail.com>
2022-08-20 15:13:00 +0800

    chore(Kevin): 增加配置项tabs标签鉴权以及多配置项的多语言配置

--- src/pages/settings/system/index.tsx
+++ src/pages/settings/system/index.tsx
@@ -1,6 +1,5 @@
 /** @format */
 
-import { useRequest } from 'umi';
 import { message, Tabs } from 'antd';
 import { list, saveConfig } from '../service';
 import { ApiSettings } from './components/cloud';
@@ -8,12 +7,15 @@ import { SiteSettings } from './components/site';
 import { useLayoutEffect, useState } from 'react';
 import { BaiduSettings } from './components/baidu';
 import { OtherSettings } from './components/other';
+import { useAccess, useRequest, useIntl } from 'umi';
 import { ContactSettings } from './components/contact';
 import { PageContainer } from '@ant-design/pro-layout';
 import { MailSettings } from '@/pages/settings/system/components/mail';
 
 export default () => {
+    const intl = useIntl();
     const { TabPane } = Tabs;
+    const access = useAccess();
     // Tab布局
     type TabPosition = 'top' | 'left';
     // 设置布局方式
@@ -25,12 +27,12 @@ export default () => {
 
     /** tab菜单列表 */
     const tabItem: Record<string, string>[] = [
-        { key: '1', pane: '网站配置' },
-        { key: '2', pane: '百度配置' },
-        { key: '3', pane: '存储配置' },
-        { key: '4', pane: '联络方式' },
-        { key: '5', pane: '邮件配置' },
-        { key: '6', pane: '其它配置' },
+        { key: '1', mark: 'website', pane: intl.formatMessage({ id: 'pages.settings.website' }) },
+        { key: '2', mark: 'baidu', pane: intl.formatMessage({ id: 'pages.settings.baidu' }) },
+        { key: '3', mark: 'storage', pane: intl.formatMessage({ id: 'pages.settings.storage' }) },
+        { key: '4', mark: 'contact', pane: intl.formatMessage({ id: 'pages.settings.contact' }) },
+        { key: '5', mark: 'email', pane: intl.formatMessage({ id: 'pages.settings.email' }) },
+        { key: '6', mark: 'other', pane: intl.formatMessage({ id: 'pages.settings.other' }) },
     ];
 
     /** 设置tab模式 */
@@ -88,11 +90,14 @@ export default () => {
     return (
         <PageContainer>
             <Tabs tabPosition={position} onTabClick={key => run({ type: key })}>
-                {tabItem.map(item => (
-                    <TabPane key={item.key} tab={item.pane}>
-                        {renderView(item.key)}
-                    </TabPane>
-                ))}
+                {tabItem.map(
+                    item =>
+                        access.btnFilter(item.mark) && (
+                            <TabPane key={item.key} tab={item.pane}>
+                                {renderView(item.key)}
+                            </TabPane>
+                        ),
+                )}
             </Tabs>
         </PageContainer>
     );
