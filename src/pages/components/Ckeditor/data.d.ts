d3bbf96cdd4f62047e6a6559265ac841fe641011
Kevin <wcdma020@hotmail.com>
2022-08-24 16:15:26 +0800

    chore(Kevin): 新增uploadPath参数属性

--- src/pages/components/Ckeditor/data.d.ts
+++ src/pages/components/Ckeditor/data.d.ts
@@ -1,12 +1,16 @@
+/** @format */
+
 import type { BaseType } from 'antd/lib/typography/Base';
 
 export interface stateData {
-  saving: boolean;
-  content: string;
-  defaultType: BaseType;
-  defaultStatus: string;
+    saving: boolean;
+    content: string;
+    uploadPath: string;
+    defaultType: BaseType;
+    defaultStatus: string;
 }
 export interface parentProps {
-  content?: string;
-  setContent: (contents: string) => void;
+    content?: string;
+    uploadPath?: string;
+    setContent: (contents: string) => void;
 }
