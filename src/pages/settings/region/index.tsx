c8b74044564d8f5a1e3b45baf4fbb9cdfa5ee28a
Kevin <wcdma020@hotmail.com>
2022-08-24 20:58:02 +0800

    chore(Kevin): 调整表格scroll滚动逻辑

--- src/pages/settings/region/index.tsx
+++ src/pages/settings/region/index.tsx
@@ -237,7 +237,6 @@ export default () => {
                 request={tableData}
                 editableFormRef={formRef}
                 recordCreatorProps={false}
-                scroll={resize.tableScroll}
                 editable={{
                     editableKeys,
                     type: 'multiple',
@@ -251,6 +250,7 @@ export default () => {
                     expandedRowKeys: expandedRowKey,
                     onExpand: (expanded, record) => handleExpand(expanded, record),
                 }}
+                scroll={1920 >= window.innerWidth ? { x: 1300, y: 600 } : resize.tableScroll}
                 rowSelection={{
                     checkStrictly: false,
                     selections: [Table.SELECTION_ALL, Table.SELECTION_INVERT, Table.SELECTION_NONE],
