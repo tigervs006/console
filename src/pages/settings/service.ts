caefe12281667e1cde011acc44fa5f46432eee36
Kevin <wcdma020@hotmail.com>
2022-09-01 09:20:54 +0800

    chore(Kevin): 调整获取行政区域列表使用接口缓存

--- src/pages/settings/service.ts
+++ src/pages/settings/service.ts
@@ -14,8 +14,8 @@ export const saveConfig = (data: Record<string, any>) => postData('/system/save'
 /** 单个/批量删除 */
 export const remove = (data: Record<'id', number | number[]>) => postData('/region/del', data);
 
-/** 获取地区列表 */
-export const fetchRegionData = (params: Record<string, any>) => getData('/region/list', params);
-
 /** 获取操作日志列表 */
 export const fetchSystemLogs = (params: Record<string, any>) => getData('/system/record', params);
+
+/** 获取地区列表 */
+export const fetchRegionData = (params: Record<string, any>) => getData('/region/list', params, { useCache: true });
