099729c93ff1d19d98f220271c174eef95fa117d
Kevin <wcdma020@hotmail.com>
2022-09-10 02:25:18 +0800

    feat(Kevin): 新增自动刷新token逻辑

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,10 @@
 /** @format */
 
-import { history } from 'umi';
 import Footer from '@/components/Footer';
 import { sortDesc } from '@/extra/utils';
 import { message, Button, Result } from 'antd';
 import { loopMenuItem } from './extra/iconsMap';
+import { request as umiRequest, history } from 'umi';
 import RightContent from '@/components/RightContent';
 import type { RequestOptionsInit } from 'umi-request';
 import defaultSettings from '../config/defaultSettings';
@@ -98,24 +98,90 @@ export const layout: RunTimeLayoutConfig = ({ initialState, setInitialState }) =
     };
 };
 
-/* 请求前拦截器 */
-const authHeaderInterceptor = (url: string, options: RequestOptionsInit) => {
+/* 是否正在请求 */
+let isPending = false;
+/* 正在等待的请求 */
+const _cacheRequest: (() => void)[] = [];
+const authHeaderInterceptor: any = (url: string, options: RequestOptionsInit) => {
+    /* 如果是登录，不做处理 */
+    if (url === '/console/public/login') {
+        return {
+            url: url,
+            options: options,
+        };
+    }
+    /* 如果是刷新，不做处理且跳过默认的异常处理 */
+    if (url === '/console/public/refresh_token') {
+        return {
+            url: url,
+            options: { ...options, skipErrorHandler: true },
+        };
+    }
+    const accessToken = localStorage?.getItem('access_token') ?? '';
+    const refreshToken = localStorage?.getItem('refresh_token') ?? '';
+    const expiresAt: number = Number(localStorage?.getItem('expiresAt') ?? 0);
+    if (accessToken && refreshToken) {
+        const nowTime: number = new Date().getTime();
+        const authHeader = { Authorization: accessToken };
+        /* access_token过期 */
+        if (nowTime >= expiresAt) {
+            if (!isPending) {
+                isPending = true;
+                if (refreshToken) {
+                    /* 调用refresh_token */
+                    umiRequest('/public/refresh_token', { method: 'post', headers: authHeader, data: { refresh_token: refreshToken } })
+                        .then(res => {
+                            const { success, data } = res;
+                            if (success && data) {
+                                const localItem = [
+                                    { uid: data?.info?.uid },
+                                    { gid: data?.info?.gid },
+                                    { user: data?.info?.name },
+                                    { refresh_token: refreshToken },
+                                    { expiresAt: data?.info?.expiresAt },
+                                    { access_token: data?.info?.access_token },
+                                ];
+                                /* 写入localStorage */
+                                localItem.forEach(item => {
+                                    for (const idx in item) {
+                                        localStorage.setItem(idx, item[idx]);
+                                    }
+                                });
+                            }
+                        })
+                        .finally(() => {
+                            /* 正在请求的队列 */
+                            isPending = false;
+                            _cacheRequest.map(req => req());
+                        });
+                }
+            }
+            /* 刷新token返回结果后再resolve */
+            return new Promise(resolve => {
+                _cacheRequest.push(() => {
+                    /* 添加请求头 */
+                    resolve({
+                        url: `${url}`,
+                        options: {
+                            ...options,
+                            interceptors: true,
+                            headers: { Authorization: localStorage?.getItem('access_token') ?? '' },
+                        },
+                    });
+                });
+            });
+        }
+    }
     return {
         url: url,
-        options: {
-            ...options,
-            interceptors: true,
-            headers: {
-                Authorization: localStorage.getItem?.('Authorization') ?? '',
-            },
-        },
+        options: { ...options, interceptors: true, headers: { Authorization: accessToken } },
     };
 };
 
 /* 响应后拦截器 */
 const ResponseInterceptors = async (response: Response) => {
     const result = await response.clone().json();
-    if (401 === result.status) {
+    if (401 == result.status) {
         localStorage.clear();
         history.push(loginPath);
     }
