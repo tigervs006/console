4562ef5b8898a0b46b4cff743e5eaec1a40f94b6
Kevin <wcdma020@hotmail.com>
2022-10-09 20:58:43 +0800

    chore(Kevin): 新增cateData全局数据

--- src/models/attach.ts
+++ src/models/attach.ts
@@ -1,8 +1,23 @@
 /** @format */
 
 import { useState } from 'react';
+import { useRequest } from 'umi';
+import type { cateDataItem } from '@/pages/components/Attach/data';
+import { cate as fetchCate } from '@/pages/components/Attach/services';
+
+const defaultCateOptions = [{ id: 0, pid: 0, path: '0', name: '根目录', ename: 'root', create_time: '2022-10-10' }];
 
 export default () => {
-    const [currentKey, setCurrentKey] = useState<string>('all');
-    return { currentKey, setCurrentKey };
+    const [currentKey, setCurrentKey] = useState<number>(0);
+    const [cateData, setCateData] = useState<cateDataItem[]>(defaultCateOptions);
+    useRequest(fetchCate, {
+        defaultParams: [{ pid: 0 }],
+        onSuccess: (res: Record<string, any>) => setCateData(prev => prev.concat(res?.list)),
+    });
+    return {
+        cateData,
+        currentKey,
+        setCateData,
+        setCurrentKey,
+    };
 };
