85f3c5a3b6c32b0b36833ead9a8f28c52245551f
Kevin <wcdma020@hotmail.com>
2022-10-09 21:06:49 +0800

    chore(Kevin): 重构Attach组件

--- src/pages/components/Attach/index.tsx
+++ src/pages/components/Attach/index.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import { Row, Col } from 'antd';
 import { Previews } from './Previews';
 import { Directory } from './Directory';
-import type { DataNode } from 'antd/es/tree';
 import type { Gutter } from 'antd/es/grid/row';
 
 export const Attach: React.FC<{
@@ -15,72 +14,14 @@ export const Attach: React.FC<{
     const gutter = props?.gutter ?? [32, 32];
     const previewSpan = props?.previewSpan ?? 20;
     const directorySpan = props?.directorySpan ?? 4;
-    const defaultData: DataNode[] = [
-        {
-            key: 'all',
-            title: '全部人类',
-        },
-        {
-            key: 'adult',
-            title: '成年人',
-            children: [
-                {
-                    key: 'man',
-                    title: '男人',
-                    children: [
-                        {
-                            key: 'father',
-                            title: '父亲',
-                        },
-                    ],
-                },
-                {
-                    key: 'woman',
-                    title: '女人',
-                    children: [
-                        {
-                            key: 'mother',
-                            title: '母亲',
-                        },
-                    ],
-                },
-            ],
-        },
-        {
-            key: 'juveniles',
-            title: '未成年人',
-            children: [
-                {
-                    key: 'boy',
-                    title: '男孩',
-                    children: [
-                        {
-                            key: 'son',
-                            title: '儿子',
-                        },
-                    ],
-                },
-                {
-                    key: 'girl',
-                    title: '女孩',
-                    children: [
-                        {
-                            key: 'daughter',
-                            title: '女儿',
-                        },
-                    ],
-                },
-            ],
-        },
-    ];
 
     return (
         <Row gutter={gutter} style={{ padding: '16px' }}>
             <Col span={directorySpan}>
-                <Directory defaultData={defaultData} />
+                <Directory />
             </Col>
             <Col span={previewSpan}>
-                <Previews directory={defaultData} />
+                <Previews />
             </Col>
         </Row>
     );
