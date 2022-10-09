60082d1e0b8269925cddd016af42f4b4b46f0b58
Kevin <wcdma020@hotmail.com>
2022-10-09 23:41:45 +0800

    chore(Kevin): 调整根目录的select为disabled禁用状态

--- src/models/attach.ts
+++ src/models/attach.ts
@@ -5,7 +5,17 @@ import { useRequest } from 'umi';
 import type { cateDataItem } from '@/pages/components/Attach/data';
 import { cate as fetchCate } from '@/pages/components/Attach/services';
 
-const defaultCateOptions = [{ id: 0, pid: 0, path: '0', name: '根目录', ename: 'root', create_time: '2022-10-10' }];
+const defaultCateOptions = [
+    {
+        id: 0,
+        pid: 0,
+        path: '0',
+        name: '根目录',
+        ename: 'root',
+        disabled: true,
+        create_time: '2022-10-10',
+    },
+];
 
 export default () => {
     const [currentKey, setCurrentKey] = useState<number>(0);
