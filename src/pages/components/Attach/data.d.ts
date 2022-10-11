3c7e9065197c3ed77e44ac8c4f1589de7ae1bb35
Kevin <wcdma020@hotmail.com>
2022-10-11 15:27:41 +0800

    chore(Kevin): 新增config参数类型

--- src/pages/components/Attach/data.d.ts
+++ src/pages/components/Attach/data.d.ts
@@ -6,6 +6,16 @@ export type cateDataItem = {
     path: string;
     name: string;
     ename: string;
+    dirname: string;
+    disabled?: boolean;
     create_time: string;
+    config?: {
+        size?: number;
+        crop?: number;
+        limit?: number;
+        astrict?: number;
+        aspects?: [number, number];
+        astricts?: [number, number];
+    };
     children?: cateDataItem[];
 };
