172a091a9ae95848ad824a19e7d35a409683917d
Kevin <wcdma020@hotmail.com>
2022-10-14 12:03:44 +0800

    chore(Kevin): 调整图像预览的若干参数

--- src/pages/components/ImagePreview/index.tsx
+++ src/pages/components/ImagePreview/index.tsx
@@ -3,13 +3,13 @@
 import { Image } from 'antd';
 import type { ForwardedRef } from 'react';
 import { randomString } from '@/extra/utils';
-import type { UploadFile } from 'antd/es/upload/interface';
+import type { attachDataItem } from '../Attach/data';
 import React, { useImperativeHandle, forwardRef, useState } from 'react';
 
 export const ImagePreview: React.FC<{
     curIdx?: number;
-    imgList: UploadFile[];
     ref: ForwardedRef<any>;
+    imgList: attachDataItem[];
 }> = forwardRef((props, ref) => {
     const curId = props?.curIdx ?? 0;
     const [visible, setVisible] = useState<boolean>(false);
@@ -18,7 +18,7 @@ export const ImagePreview: React.FC<{
         <div style={{ display: 'none' }}>
             <Image.PreviewGroup preview={{ visible, current: curId, onVisibleChange: value => setVisible(value) }}>
                 {props?.imgList.map(item => (
-                    <Image key={randomString(4)} src={item.url} />
+                    <Image key={randomString(4)} src={item.static_path} />
                 ))}
             </Image.PreviewGroup>
         </div>
