ab792584f21444618a88a5b9123c97dd035fa8de
Kevin <wcdma020@hotmail.com>
2022-10-14 12:06:29 +0800

    chore(Kevin): 新增若干接口方法

--- src/pages/components/Attach/services.ts
+++ src/pages/components/Attach/services.ts
@@ -1,6 +1,6 @@
 /** @format */
 
-import type { cateDataItem } from './data';
+import type { attachDataItem, cateDataItem } from './data';
 import { getData, postData } from '@/services/ant-design-pro/api';
 
 export const info = (param: { id: number }) => getData('/attach/info', param);
@@ -8,5 +8,11 @@ export const info = (param: { id: number }) => getData('/attach/info', param);
 export const cate = (param: { pid?: number }) => getData('/attach/cate', param);
 
 export const remove = (data: { id: number }) => postData('/attach/delete', data);
+
+export const move = (data: { id: number[]; pid: number }) => postData('/attach/move', data);
+
+export const deletes = (data: { attach: attachDataItem | attachDataItem[] }) => postData('/attach/remove', data);
+
+export const list = (param: { id?: number; current: number; pageSize: number }) => getData('/attach/list', param);
 
 export const save = (data: Pick<cateDataItem, 'id' | 'pid' | 'path' | 'name' | 'ename'>) => postData('/attach/save', data);
